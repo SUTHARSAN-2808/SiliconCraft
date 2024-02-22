a4a01b059603a0ad09e5aae3106af34c22bece2d
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-02-22 20:31:23 +0530

    Update full_adder.v

--- Verilog/Day1/FULL_ADDER2/full_adder.v
+++ Verilog/Day1/FULL_ADDER2/full_adder.v
@@ -1,1 +1,19 @@
+module full_adder(input a,
+                  input b,
+                  input cin,
+                  output sum,
+                  output cout);
+  
+  wire x,y,z;
+  halfadder ha1(.a(a),.b(b),.s(x),.c(y));
+  halfadder ha2(.a(x),.b(cin),.s(sum),.c(z));
+  or or1(cout,z,y);
+  endmodule
 
+    module halfadder(input a,
+                      input b,
+                      output s,
+                      output c);
+      xor xor1(s,a,b);
+      and and1(c,a,b);
+    endmodule
