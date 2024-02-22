01cd47c0bbd3173049b7b542290e750857cebfe0
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-02-22 15:25:04 +0530

    Update nor_tb.v

--- Verilog/Day1/NOR_GATE/nor_tb.v
+++ Verilog/Day1/NOR_GATE/nor_tb.v
@@ -1,1 +1,24 @@
+module nor_tb;
+  reg x,y;
+  wire z;
+//instantiate the design with test bench
+  norgate nor2(.x(x),.y(y),.z(z));
+//dumping the values 
+  initial begin
+  $dumpfile("norgate.vcd");
+  $dumpvars(1,nor_tb);
+  end
+//Entering the values
+  initial begin
+    $monitor("mtime %d x=%d y=%d z=%d",$time,x,y,z);
+  x=0;y=0;#4
+  x=0;y=1;#41
+  x=1;y=0;#90
+  x=1;y=1;#4
+    $finish;
+  end
+endmodule
 
+    
+    
+  
