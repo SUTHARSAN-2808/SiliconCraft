6e05b1d73cda2ecfbec1836d657158ae26f09cfd
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-02-22 20:32:05 +0530

    Update full_adder_tb.v

--- Verilog/Day1/FULL_ADDER2/full_adder_tb.v
+++ Verilog/Day1/FULL_ADDER2/full_adder_tb.v
@@ -1,1 +1,36 @@
+module full_adder_tb;
+  reg a,b,cin;
+  wire sum,cout;
+  
+ //instantiate the design with testbench
+  full_adder fa1(.a(a),.b(b),.cin(cin),.sum(sum),.cout(cout));
+ //dumping the values of design into testbench
+  initial begin
+  $dumpfile("full_adder.vcd");
+  $dumpvars(1,full_adder_tb);
+  end
+  //Entering the values
+  initial begin
+    $monitor("mtime %d a=%d b=%d cin=%d sum=%d cout=%d",$time,a,b,cin,sum,cout);
+    a=0;b=0;cin=0;#4
+   
+     a=0;b=0;cin=1;#4
+    
+     a=0;b=1;cin=0;#4
 
+     a=0;b=1;cin=1;#4
+  
+  
+     a=1;b=0;cin=0;#4
+  
+  
+     a=1;b=0;cin=1;#4
+    
+      a=1;b=1;cin=1;#4
+     $monitor("mtime %d a=%d b=%d cin=%d sum=%d cout=%d",$time,a,b,cin,sum,cout);
+    $finish;
+  end
+endmodule
+  
+  
+  
