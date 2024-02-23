88aa078ba83b941dbc7705a7e31dfd8365bc0d46
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-02-23 12:44:43 +0530

    Update demux1_4.v

--- Verilog/Day2/DEMUX/demux1:4/demux1_4.v
+++ Verilog/Day2/DEMUX/demux1:4/demux1_4.v
@@ -1,1 +1,17 @@
+module demux1_4(input x,
+                input s1,
+                input s0,
+                output d0,
+                 output d1,
+                 output d2,
+                 output d3
+               );
+  wire s11,s00;
+  not not1(s11,s1);
+  not not2(s00,s0);
+  and and1(d0,s11,s00,x);
+  and and2(d1,s11,s0,x);
+  and and3(d2,s1,s00,x);
+  and and4(d3,s1,s0,x);
+endmodule
 
