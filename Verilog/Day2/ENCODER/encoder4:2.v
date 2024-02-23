16cd044af32daffc03006d452f2522ad58a73a48
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-02-23 16:50:55 +0530

    Update encoder4:2.v

--- Verilog/Day2/ENCODER/encoder4:2.v
+++ Verilog/Day2/ENCODER/encoder4:2.v
@@ -1,1 +1,10 @@
-
+module encoder(input a,
+               input b,
+               input c,
+               input d,
+               output y,
+               output z);
+  or or1(y,a,b);
+  or or2(z,c,a);
+endmodule
+  
