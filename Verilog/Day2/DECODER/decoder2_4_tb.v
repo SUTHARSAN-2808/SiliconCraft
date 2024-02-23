f83f48b66109819cae14e8fabb6288b7bda4c361
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-02-23 16:57:20 +0530

    Update decoder2_4_tb.v

--- Verilog/Day2/DECODER/decoder2_4_tb.v
+++ Verilog/Day2/DECODER/decoder2_4_tb.v
@@ -1,1 +1,21 @@
 
+module decoder_tb;
+  reg a,b;
+  wire y0,y1,y2,y3;
+//instantiate design with the testbench
+  decoder decoder1(.a(a),.b(b),.y0(y0),.y1(y1),.y2(y2),.y3(y3));
+//dumping 
+  initial begin
+    $dumpfile("decoder.vcd");
+    $dumpvars(1,decoder_tb);
+  end
+//entering the values
+  initial begin
+    $monitor("a=%d b=%d y0=%d y1=%d y2=%d y3=%d",a,b,y0,y1,y2,y3);
+    a=0;b=0;#2
+    a=0;b=1;#2
+    a=1;b=0;#2
+    a=1;b=1;#2
+    $finish;
+  end 
+endmodule
