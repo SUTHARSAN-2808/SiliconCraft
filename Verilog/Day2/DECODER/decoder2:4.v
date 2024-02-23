62b98c1fe19baa676c6bd0819a067d09a952722f
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-02-23 16:56:28 +0530

    Update decoder2:4.v

--- Verilog/Day2/DECODER/decoder2:4.v
+++ Verilog/Day2/DECODER/decoder2:4.v
@@ -1,1 +1,14 @@
-
+module decoder(input a,
+              input b,
+              output y0,
+              output y1,
+              output y2,
+               output y3);
+wire aa,bb;
+  not not1(aa,a);
+  not not2(bb,b);
+  and and1(y0,aa,bb);
+  and and2(y1,aa,b);
+  and and3(y2,a,bb);
+  and and4(y3,a,b);
+endmodule
