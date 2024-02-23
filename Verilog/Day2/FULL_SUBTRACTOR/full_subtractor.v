4a051c70faceacac43133c373ece5794a169d49a
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-02-23 09:44:14 +0530

    Update full_subtractor.v

--- Verilog/Day2/FULL_SUBTRACTOR/full_subtractor.v
+++ Verilog/Day2/FULL_SUBTRACTOR/full_subtractor.v
@@ -1,1 +1,21 @@
-
+module full_sub(input a,
+                input b,
+                input c,
+                output diff,
+                output bor);
+  wire u,v,w;
+  half_sub ha_s1(.m(a),.n(b),.d1(u),.b1(v));
+  half_sub ha_s2(.m(u),.n(c),.d1(diff),.b1(w));
+  or or2(bor,v,w);
+endmodule
+module half_sub(input m,
+                input n,
+                output d1,
+                output b1);
+  wire w1;
+  xor xor1(d1,m,n);
+  not not1(w1,m);
+  and and1(b1,w1,n);
+endmodule
+  
+  
