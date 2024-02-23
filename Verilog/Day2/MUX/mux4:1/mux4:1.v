45d371bcba3551500062ce3d454d66023b106a4e
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-02-23 11:36:39 +0530

    Update mux4:1.v

--- Verilog/Day2/MUX/mux4:1/mux4:1.v
+++ Verilog/Day2/MUX/mux4:1/mux4:1.v
@@ -1,1 +1,16 @@
-
+module mux(input a,
+           input b,
+           input c,
+           input d,
+           input s1,
+           input s0,
+           output y);
+  wire s11,s00,a1,b1,c1,d1;
+  not not1(s11,s1);
+  not not2(s00,s0);
+  and and1(a1,s11,s00,a);
+  and and2(b1,s11,s0,b);
+  and and3(c1,s1,s00,c);
+  and and4(d1,s1,s0,d);
+  or or1(y,a1,b1,c1,d1);
+endmodule
