f100707499e64a0c8cb294b14235d54af7079604
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-02-23 11:39:57 +0530

    Update mux2:1_tb.v

--- Verilog/Day2/MUX/mux2:1/mux2:1_tb.v
+++ Verilog/Day2/MUX/mux2:1/mux2:1_tb.v
@@ -1,1 +1,18 @@
-
+module mux2_1_tb;
+  reg a,b,s;
+  wire y;
+//instantiate design with testbench
+  mux2_1 mux(.a(a),.b(b),.s(s),.y(y));
+//dumping the values
+  initial begin
+    $dumpfile("mux2_1.vcd");
+    $dumpvars(1,mux2_1_tb);
+  end
+//enter the values
+  initial begin
+    a=0;b=1;s=0;#9
+    $display("time=%d a=%d b=%d s=%d y=%d",$time,a,b,s,y);
+    a=0;b=1;s=1;#9
+    $display("time=%d a=%d b=%d s=%d y=%d",$time,a,b,s,y);
+  end
+endmodule
