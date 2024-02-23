6c2a720fcc4558184cc63312206e54f0b9228491
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-02-23 11:38:48 +0530

    Update mux2:1.v

--- Verilog/Day2/MUX/mux2:1/mux2:1.v
+++ Verilog/Day2/MUX/mux2:1/mux2:1.v
@@ -1,1 +1,11 @@
-
+module mux2_1(input a,
+              input b,
+              input s,
+              output y);
+  wire s0,a1,b1;
+  not not1(s0,s);
+  and and1(a1,a,s0);
+  and and2(b1,b,s);
+  or or1(y,a1,b1);
+endmodule
+  
