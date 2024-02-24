fc67f99ff20cdbd85774d26a3bd1e09d597da0f3
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-02-24 20:48:46 +0530

    Update comparator.v

--- Verilog/Day3/COMPARATOR/comparator.v
+++ Verilog/Day3/COMPARATOR/comparator.v
@@ -1,1 +1,20 @@
 
+module comparator(a,b,Aeqb,Agtb,Altb);
+  input[3:0] a,b;
+  output Aeqb,Agtb,Altb;
+   reg Aeqb, Agtb, Altb;              
+
+  always @(a or b)
+    begin
+      Aeqb = 0;
+      Agtb = 0;
+      Altb = 0;
+      if(a == b )
+        Aeqb=1;
+      else if(a > b)
+        Agtb=1;
+      else
+        Altb=1;
+    end
+endmodule
+
