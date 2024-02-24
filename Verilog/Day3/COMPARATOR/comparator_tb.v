41346ce9f9970df1957ad54de35950efe82013f8
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-02-24 20:49:39 +0530

    Update comparator_tb.v

--- Verilog/Day3/COMPARATOR/comparator_tb.v
+++ Verilog/Day3/COMPARATOR/comparator_tb.v
@@ -1,1 +1,21 @@
-
+module comparator_tb;
+  reg [3:0] a,b;
+  wire Aeqb,Agtb,Altb;
+//instantiate the design with testbench
+  comparator comp(.a(a),.b(b),.Aeqb(Aeqb),.Agtb(Agtb),.Altb(Altb));
+  always #4 a=$random%16;
+  always #4 b=$random%16;
+//Dumping the values
+  initial begin
+    $dumpfile("comparator.vcd");
+    $dumpvars(1,comparator_tb);
+  end
+//Entering the values
+  initial begin
+    a=4'b0000;#10
+    $display("a=%d b=%d Aeqb=%d Agtb=%d Altb%d",a,b,Aeqb,Agtb,Altb);
+    b=4'b0000;#10
+    $display("a=%d b=%d Aeqb=%d Agtb=%d Altb%d",a,b,Aeqb,Agtb,Altb);
+    $finish;
+  end
+endmodule
