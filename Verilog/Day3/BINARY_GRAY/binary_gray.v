a8a2bd5b20eafe1f800ee8cf113fbe9978e8db41
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-02-24 10:38:11 +0530

    Update binary_gray.v

--- Verilog/Day3/BINARY_GRAY/binary_gray.v
+++ Verilog/Day3/BINARY_GRAY/binary_gray.v
@@ -1,1 +1,13 @@
-
+module bin_gray(input a,
+                input b,
+                input c,
+                input d,
+                output g1,
+                output g2,
+                output g3,
+                output g4);
+  xor xor1(g4,c,d);
+  xor xor2(g3,b,c);
+  xor xor3(g2,a,b);
+  buf buf1(g1,a);
+endmodule
