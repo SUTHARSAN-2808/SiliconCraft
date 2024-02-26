9c00d3bc6cb8b283efa9f141b454159ac71afb77
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-02-26 09:25:14 +0530

    Update bcd_excess3.v

--- Verilog/Day5/BCD_EXCESS3/bcd_excess3.v
+++ Verilog/Day5/BCD_EXCESS3/bcd_excess3.v
@@ -1,1 +1,13 @@
-
+module bcd_excess3(input a,
+      input b,
+      input c,
+      input d,
+      output w,
+      output x,
+      output y,
+       output z);
+  assign w=((a | (b & c) | (b & d)));
+  assign x=((~b & c) | (~b & d) | (b & ~c & ~d));
+  assign y=((c & d) | (~c & ~d));
+  assign z=(~d);
+endmodule
