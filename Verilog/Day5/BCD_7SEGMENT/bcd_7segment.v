be38dc25dd0d43e09b518900fed7591e633fcb27
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-02-26 10:02:12 +0530

    Update bcd_7segment.v

--- Verilog/Day5/BCD_7SEGMENT/bcd_7segment.v
+++ Verilog/Day5/BCD_7SEGMENT/bcd_7segment.v
@@ -1,1 +1,20 @@
-
+module bcd_7segment(input a,
+                    input b,
+                    input c,
+                    input d,
+                    output A,
+                    output B,
+                    output C,
+                    output D,
+                    output E,
+                    output F,
+                    output G);
+  assign A=((~b & ~d) | c | (b & d) | a);
+  assign B=((~b) | (~c & ~d) | (c & d));
+  assign C= (~c | d | b);
+  assign D=((~b & ~d) | (~b & c) | (b & ~c & d) | (c & ~d) | a);
+  assign E=((~b & ~d) | (c & ~d));
+  assign F=((~c & ~d) | (b & ~c) | (b & ~d) | a);
+  assign G=((~b & c) | (b & ~c) | a | (b & ~d));
+endmodule
+                    
