b573e71da8f74f1d64701b646583cbf662440c15
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-02-26 11:04:41 +0530

    Update d_latch.v

--- Verilog/Day5/D_LATCH/d_latch.v
+++ Verilog/Day5/D_LATCH/d_latch.v
@@ -1,1 +1,13 @@
-
+module d_latch(input data,
+               input en,
+              input reset,
+               output  reg q);
+  always @(en,data,reset)
+   
+    if(reset==1)begin
+        q<=0;
+    end
+    else if(en==1)begin
+        q<=data;
+    end
+endmodule
