2da05d70f732cd5ecd45e4417889c2162e8e1e59
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-02-26 16:10:39 +0530

    Update d_flipflop.v

--- Verilog/Day5/D_FLIPFLOP/d_flipflop.v
+++ Verilog/Day5/D_FLIPFLOP/d_flipflop.v
@@ -1,1 +1,14 @@
+module d_flipflop(input d,
+                 input clk,
+                 input reset,
+                  output reg q);
+  always @(posedge clk)
+    if(reset == 1)begin
+       q <= 0;
+    end
+    else 
+       q <= d;
+   // end 
+endmodule
+      
 
