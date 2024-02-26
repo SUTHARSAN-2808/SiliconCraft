0c7fa3a3776d8a97b729ed856c66b91dac789159
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-02-26 11:05:22 +0530

    Update d_latch_tb.v

--- Verilog/Day5/D_LATCH/d_latch_tb.v
+++ Verilog/Day5/D_LATCH/d_latch_tb.v
@@ -1,1 +1,22 @@
-
+module d_latch_tb;
+  reg data,en,reset;
+  wire q;
+//instantiate 
+  d_latch(.data(data),.en(en),.reset(reset),.q(q));
+//dumping
+  initial begin
+    $dumpfile("d_latch.vcd");
+    $dumpvars(1,d_latch_tb);
+  end
+//Entering the values
+  initial begin
+    $monitor("data=%d en=%d reset=%d q=%d ",data,en,reset,q);
+    data=1;
+    en=0;
+    #6 reset=1;
+  #150 $finish;
+  end
+  always #25 reset=~reset;
+  always #20 en=~en;
+  always #15 data=~data;
+endmodule
