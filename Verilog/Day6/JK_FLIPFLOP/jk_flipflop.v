7a2ce5587da8dc035e00862d51bac42c582c29ac
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-02-27 16:39:56 +0530

    Update jk_flipflop.v

--- Verilog/Day6/JK_FLIPFLOP/jk_flipflop.v
+++ Verilog/Day6/JK_FLIPFLOP/jk_flipflop.v
@@ -1,1 +1,27 @@
+module jk_flipflop(input j,
+                  input k,
+                  input clk,
+                  input reset,
+                  output reg q,
+                   output reg qbar);
+  always @(posedge clk)
+    if(reset)begin
+      q=0;
+     
+    end
+  else if(j == 0 && k == 0)begin
+    q=q;  
+  end
+    else if(j == 0 && k == 1)begin
+      q=0;
+    end
+  else if(j == 1 && k ==0)begin
+    q=1;   
+  end
+  else 
+    q=~q;
+   assign qbar=~q;
+  
+endmodule
+     
 
