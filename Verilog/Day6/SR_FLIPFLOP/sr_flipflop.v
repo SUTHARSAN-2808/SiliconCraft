7351da7a57a1be6477d529a11a542df006cad5be
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-02-27 14:48:45 +0530

    Update sr_flipflop.v

--- Verilog/Day6/SR_FLIPFLOP/sr_flipflop.v
+++ Verilog/Day6/SR_FLIPFLOP/sr_flipflop.v
@@ -1,1 +1,25 @@
 
+module sr_flipflop(input s,
+                   input r,
+                  input clk,
+                  output reg q,
+                   output reg qbar);
+  always @(posedge clk)
+    if (r) begin
+        q <= 1'b0;
+    end 
+    else if(s==0 && r==0)begin
+    q<=q;
+    end
+    else if(s==0 && r==1)begin
+    q<=0;
+    end
+      else if(s==1&& r==0)begin
+    q<=1;
+    end
+    else
+      q<=1;   
+      assign qbar=~q;
+    
+endmodule
+  
