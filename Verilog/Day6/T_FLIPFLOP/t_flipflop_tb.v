eddf6589c4b529649ad5f300967b007f156113fc
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-02-27 07:42:32 +0530

    Update t_flipflop_tb.v

--- Verilog/Day6/T_FLIPFLOP/t_flipflop_tb.v
+++ Verilog/Day6/T_FLIPFLOP/t_flipflop_tb.v
@@ -1,1 +1,23 @@
-
+module t_flipflop_tb;
+  reg d,clk,reset;
+  wire q;
+//instantiate
+  t_flipflop tff(.d(d),.clk(clk),.reset(reset),.q(q));
+//dumping the values
+  initial begin
+    $dumpfile("t_flipflop.vcd");
+    $dumpvars(1,t_flipflop_tb);
+  end
+//Entering the values
+  initial begin
+    $monitor("d=%d clk=%d reset=%d q=%d",d,clk,reset,q);
+    d=0;
+    clk=1;
+    #4 reset=1;
+    #400 $finish;
+  end
+  always #15 d=~d;
+  always #10 clk=~clk;
+  always #50 reset=~reset;
+endmodule
+           
