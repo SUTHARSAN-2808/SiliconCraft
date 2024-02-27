5b401aad8b1ba77f75a4cd45d6b36ee41d5d35fc
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-02-27 23:06:37 +0530

    Update async_d_flipflop.v

--- Verilog/Day6/ASYNC_D_FLIPFLOP/async_d_flipflop.v
+++ Verilog/Day6/ASYNC_D_FLIPFLOP/async_d_flipflop.v
@@ -1,1 +1,14 @@
 
+module d_flipflop(input d,
+                 input clk,
+                 input reset,
+                  output reg q);
+  always @(posedge clk or negedge reset)
+    if(~reset)begin
+       q <= 0;
+    end
+    else 
+       q <= d;
+   // end 
+endmodule
+      
