dd8db602a43eea44c3824d7f9e20ba589a875788
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-02-27 15:50:53 +0530

    Update t_flipflop.v

--- Verilog/Day6/T_FLIPFLOP/t_flipflop.v
+++ Verilog/Day6/T_FLIPFLOP/t_flipflop.v
@@ -1,13 +1,20 @@
 module t_flipflop(input d,
                  input clk,
                  input reset,
-                  output reg q);
+                  output reg q,
+                 output reg qbar);
   always @(negedge reset or posedge clk)
     if(!reset)begin
-      q = 0;
+    q = 0;
+    qbar =~q;
     end
-   else
-     q =~d;
+  else if (d == 0)begin
+    q=q;
+   qbar =~q;
+  end
+  else if(d == 1)begin
+    q =~q;
+     qbar =~q;
+  end
 endmodule
     
-
