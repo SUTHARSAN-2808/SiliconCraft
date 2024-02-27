0f0f4693c90a7a9baef757e76b3c10304c4b66f1
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-02-27 23:28:44 +0530

    Update async_jk_flipflop.v

--- Verilog/Day6/ASYNC_JK_FLIPFLOP/async_jk_flipflop.v
+++ Verilog/Day6/ASYNC_JK_FLIPFLOP/async_jk_flipflop.v
@@ -1,1 +1,28 @@
+module jk_flipflop(input j,
+                  input k,
+                  input clk,
+                  input reset,
+                  output reg q,
+                   output reg qbar);
+  always @(posedge clk or negedge reset)
+    if(~reset)begin
+      q=0;
+     
+    end
+  else if(j == 0 && k == 0)begin
+    q=q;  
+  end
+    else if(j == 0 && k == 1)begin
+      q=0;
+    end
+  else if(j == 1 && k ==0)begin
+    q=1;   
+  end
+  else 
+    q=1'bx;
+   assign qbar=~q;
+  
+endmodule
+     
+
 
