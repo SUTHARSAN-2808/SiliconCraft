149a1e195e575c2a5454f0813aabc3c7b1ac655e
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-02-28 22:46:30 +0530

    Update logical_operation_tb.v

--- Verilog/Day7/LOGICAL_OPERATIONS/logical_operation_tb.v
+++ Verilog/Day7/LOGICAL_OPERATIONS/logical_operation_tb.v
@@ -1,1 +1,18 @@
+module logical_operator_tb;
+  reg [1:0] a,b;
+  wire [1:0] y1,y2,y3,y4;
+//instantiate
+  logical_operator aa(a,b,y1,y2,y3);
+//Dumping the values
+  initial begin
+    $dumpfile("logical_operator.vcd");
+    $dumpvars(1);
+  end
+//Entering the values
+  initial begin
+    $monitor("a=%b b=%b y1=%b y2=%b y3=%b",a,b,y1,y2,y3);
+    a=1;b=0;#4
+    $finish;
+  end
+endmodule
 
