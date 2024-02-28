8d85c9bb15a4c759267bcfc31ee8ecbabe339667
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-02-28 09:23:49 +0530

    Update sync_d_flipflop_tb.v

--- Verilog/Day7/SYNC_D_FLIPFLOP/sync_d_flipflop_tb.v
+++ Verilog/Day7/SYNC_D_FLIPFLOP/sync_d_flipflop_tb.v
@@ -1,1 +1,26 @@
 
+module d_flipflop_tb;
+  reg d,clk,reset;
+  wire q;
+//instantiating the design with testbench
+  d_flipflop g(.d(d),.clk(clk),.reset(reset),.q(q));
+//dumping the values
+  initial begin
+    $dumpfile("d_flipflop.vcd");
+    $dumpvars(1,d_flipflop_tb);
+  end
+//entering the values 
+  initial begin
+    $monitor("d=%d clk=%d reset=%d q=%d", d,clk,reset,q);
+    d=1;
+    clk=1;
+    #5 reset=1;
+    #10 reset=0;
+    #50 $finish;
+  end
+
+    always #5 clk=~clk;
+    always #2 reset =~reset;
+endmodule
+    
+
