fddca20b47c3909183e1ea642aaa079c8893d0d8
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-02-28 15:24:05 +0530

    Update arithmetic_operators_tb.v

--- Verilog/Day7/ARITHMETIC_OPERATORS/arithmetic_operators_tb.v
+++ Verilog/Day7/ARITHMETIC_OPERATORS/arithmetic_operators_tb.v
@@ -1,1 +1,19 @@
+module arithmetic_tb;
+  reg [2:0] a,b;
+  wire [3:0] y1,y2,y3,y4;
+//instantiate the design with testbench
+  arithmetic aa(.a(a),.b(b),.y1(y1),.y2(y2),.y3(y3),.y4(y4));
+//Dumping the values of design with testbench
+  initial begin
+    $dumpfile("arithmetic.vcd");
+    $dumpvars(1,arithmetic_tb);
+  end
+//Entering the values
+  initial begin  
+     $monitor("a=%d b=%d y1=%d y2=%d y3=%d y4=%d",a,b,y1,y2,y3,y4);    
 
+    a=010;b=001;#8   
+    $finish;
+     end       
+endmodule
+    
