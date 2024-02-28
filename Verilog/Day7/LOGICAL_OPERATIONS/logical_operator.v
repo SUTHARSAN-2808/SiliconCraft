7304fa3e6d5141b5a2e4e2267a425d9d2a11d8fd
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-02-28 22:43:08 +0530

    Update logical_operator.v

--- Verilog/Day7/LOGICAL_OPERATIONS/logical_operator.v
+++ Verilog/Day7/LOGICAL_OPERATIONS/logical_operator.v
@@ -1,1 +1,13 @@
+module logical_operator(input [1:0]a,
+                        input [1:0]b,
+                        output reg [1:0]y1,
+                        output reg [1:0]y2,
+                        output reg [1:0]y3);
+                        
+  always @(*) begin
+    assign y1=a && b;
+    assign y2= a || b;
+    assign y3=~a;
+  end
+endmodule
 
