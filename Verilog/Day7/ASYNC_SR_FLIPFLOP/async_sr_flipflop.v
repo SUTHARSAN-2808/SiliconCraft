3739638e320b73c8ccea1260c9d60c734840ba19
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-02-28 07:24:30 +0530

    Update async_sr_flipflop.v

--- Verilog/Day7/ASYNC_SR_FLIPFLOP/async_sr_flipflop.v
+++ Verilog/Day7/ASYNC_SR_FLIPFLOP/async_sr_flipflop.v
@@ -1,1 +1,25 @@
+module sr_flipflop(input s,
+                   input r,
+                  input clk,
+                   input reset,
+                  output reg q,
+                   output reg qbar);
+  always @(posedge clk or negedge reset)
+    if (~reset)
+      q=0;
+   else if(s==0 && r==0)begin
+        q<=q;
+    end
+    else if(s==0 && r==1)begin
+       q<=0;
+    end
+      else if(s==1&& r==0)begin
+      q<=1;
+    end
+    else
+     q=1'bx;
+     assign qbar=~q;
+    
+endmodule
+  
 
