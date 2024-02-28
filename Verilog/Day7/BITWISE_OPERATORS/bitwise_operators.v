577c9085c6bb77fc21bc17b269e917560f58a68c
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-02-28 21:45:17 +0530

    Update bitwise_operators.v

--- Verilog/Day7/BITWISE_OPERATORS/bitwise_operators.v
+++ Verilog/Day7/BITWISE_OPERATORS/bitwise_operators.v
@@ -1,1 +1,20 @@
+module bitwise_operators(input [5:0]a,
+                         input [5:0]b,
+                         output reg [5:0]o1,
+                         output reg [5:0]o2,
+                         output reg [5:0]o3,
+                         output reg [5:0]o4,
+                         output reg [5:0]o5);
+  always @(*) begin
+   
+       o1=a & b;
+       o2=a | b;
+       o3=a ^ b;
+       o4=~a;
+       o5=~&a;
+  end
+endmodule
+    
+    
+  
 
