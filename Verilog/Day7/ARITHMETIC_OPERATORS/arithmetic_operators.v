9d697e0af781c836e869d2c48e666d973b581bef
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-02-28 15:23:25 +0530

    Update arithmetic_operators.v

--- Verilog/Day7/ARITHMETIC_OPERATORS/arithmetic_operators.v
+++ Verilog/Day7/ARITHMETIC_OPERATORS/arithmetic_operators.v
@@ -1,1 +1,17 @@
+module arithmetic(
+    input [2:0] a,
+    input [2:0] b,
+  output reg[3:0] y1,
+  output  reg[3:0] y2,
+  output  reg[3:0] y3,
+  output  reg[3:0] y4);
+
+  
+  always@(*) begin
+     y1 = a + b;
+     y2 = b-a;
+     y3 = a * b;
+     y4 = a % b;
+  end
+endmodule
 
