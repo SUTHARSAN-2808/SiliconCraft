0ce6789fa05f9ccb0f59874a05e432d894604d1e
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-02-29 07:24:37 +0530

    Update shifting_operators.v

--- Verilog/Day8/SHIFTING_OPERATORS/shifting_operators.v
+++ Verilog/Day8/SHIFTING_OPERATORS/shifting_operators.v
@@ -1,1 +1,14 @@
+module shifting_operators(input [3:0]a,
+                          input [3:0]b,
+                          output reg[3:0]y1,
+                          output reg[3:0]y2,
+                          output reg[3:0]y3,
+                          output reg[3:0]y4);
+  always@(*)begin
+    y1=a<<1;
+    y2=a>>1;
+    y3=b<<2;
+    y4=b >> 2;
+  end
+endmodule
 
