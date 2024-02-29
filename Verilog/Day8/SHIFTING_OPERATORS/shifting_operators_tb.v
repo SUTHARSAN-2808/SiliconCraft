fcbc970ddcaa5c5df0529504020ef01fcf3de57a
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-02-29 07:43:10 +0530

    Update shifting_operators_tb.v

--- Verilog/Day8/SHIFTING_OPERATORS/shifting_operators_tb.v
+++ Verilog/Day8/SHIFTING_OPERATORS/shifting_operators_tb.v
@@ -1,8 +1,8 @@
 module shifting_operators_tb;
   reg[3:0] a,b;
-  wire[3:0] y1,y2,y3,y4;
+  wire[3:0] y1,y2,y3,y4,y5,y6;
 //Instantiate the design with testbench
-  shifting_operators so(a,b,y1,y2,y3,y4);
+  shifting_operators so(a,b,y1,y2,y3,y4,y5,y6);
 //Dumping the values
   initial begin
     $dumpfile("shifting_operator.vcd");
@@ -13,12 +13,11 @@ module shifting_operators_tb;
     repeat (4)begin
     a=$random;b=$random;#4
      
-    $display("a=%b b=%b y1=%b y2=%b y3=%b y4=%b",a,b,y1,y2,y3,y4);
+      $display("a=%b b=%b y1=%b y2=%b y3=%b y4=%b y5=%b y6=%b",a,b,y1,y2,y3,y4,y5,y6);
      
     end
     #30 $finish;
   end
 endmodule
-    
     
 
