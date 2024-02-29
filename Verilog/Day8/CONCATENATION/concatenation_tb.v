6e2fb792b7475f2d18787aac15d336b44df43607
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-02-29 08:00:26 +0530

    Update concatenation_tb.v

--- Verilog/Day8/CONCATENATION/concatenation_tb.v
+++ Verilog/Day8/CONCATENATION/concatenation_tb.v
@@ -1,1 +1,18 @@
+module concatenation_operators_tb;
+  reg [3:0] a,b;
+  wire [50:0] y1,y2,y3;
+//instantiating
+  concatenation x(a,b,y1,y2,y3);
+//Dumping 
+  initial begin
+    $dumpfile("concatenation.vcd");
+    $dumpvars(1);
+  end
+//Entering
+  initial begin
+    a=$random;b=$random;#8
+    $display("a=%b b=%b y1=%b y2=%b y3=%b",a,b,y1,y2,y3);
+    $finish;
+  end
+endmodule
 
