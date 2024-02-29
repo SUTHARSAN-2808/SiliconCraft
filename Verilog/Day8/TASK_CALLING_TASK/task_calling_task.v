f422ac90f2ae5eb8fc8e07d7a96651252e800089
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-02-29 22:31:12 +0530

    Update task_calling_task.v

--- Verilog/Day8/TASK_CALLING_TASK/task_calling_task.v
+++ Verilog/Day8/TASK_CALLING_TASK/task_calling_task.v
@@ -1,1 +1,26 @@
+module task_calling_task;
+  initial begin
+    arithmetic;
+  end
+  task arithmetic;
+    begin
+    mul(3,6);
+    div(9,3);
+    end
+  endtask
+  task mul;
+    input [4:0] a,b;
+    output [4:0] c;
+    c=a*b;
+    $display(c);
+  endtask
+  task div;
+    input [4:0] d,e;
+    output [4:0] f;
+    f=d/e;
+    $display(f);
+  endtask
+endmodule
+  
+    
 
