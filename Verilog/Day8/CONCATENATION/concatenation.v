2b64291267d9b3e4911bc84f75b5d5f7d69f6e9f
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-02-29 07:59:42 +0530

    Update concatenation.v

--- Verilog/Day8/CONCATENATION/concatenation.v
+++ Verilog/Day8/CONCATENATION/concatenation.v
@@ -1,1 +1,13 @@
+module concatenation(input [3:0]a,
+                     input [3:0]b,
+                     output reg[50:0]y1,
+                     output reg[50:0]y2,
+                     output reg[50:0]y3
+                    );
+  always @(*)begin
+    y1={a,b};
+    y2={4{a}};
+    y3={4{a,b}};
+  end
+endmodule
 
