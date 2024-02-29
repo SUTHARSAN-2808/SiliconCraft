7b4873717c24d0659a8865ea3ac4354b87946de1
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-02-29 14:47:55 +0530

    Update task.v

--- Verilog/Day8/TASK/task.v
+++ Verilog/Day8/TASK/task.v
@@ -1,1 +1,12 @@
-
+module tsk(input [4:0]a,
+           input [4:0]b,
+           output reg [4:0]add);
+  task  sk(input [4:0]ain,input [4:0]bin,output reg [4:0]x);
+    x=ain+bin; 
+    $display("add=%d",x); 
+  endtask
+   initial begin
+     sk(4,9,add);  
+     $display("add=%d",add);      
+   end
+endmodule
