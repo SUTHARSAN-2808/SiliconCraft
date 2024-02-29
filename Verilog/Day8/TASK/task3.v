cf84626fc0a5f4f1035017109951d6f17f4d75c8
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-02-29 20:27:22 +0530

    Update task3.v

--- Verilog/Day8/TASK/task3.v
+++ Verilog/Day8/TASK/task3.v
@@ -1,1 +1,13 @@
-
+module operation(input [3:0]a,
+                
+                 output reg[3:0]d);
+  task dood(input [3:0]b,output reg[3:0]c);
+    begin
+    assign  c=~b;
+    end
+    endtask
+    initial begin
+      dood(1001,d);
+      $display("ans=%b",d);
+        end
+        endmodule
