54b3c13d237cec2980760c979dc40744a571323e
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-02-29 05:37:45 +0530

    Update relational_operators.v

--- Verilog/Day8/RELATIONAL_OPERATORS/relational_operators.v
+++ Verilog/Day8/RELATIONAL_OPERATORS/relational_operators.v
@@ -1,1 +1,13 @@
+module relational_operator(input [3:0]a,
+                                  input [3:0]b,
+                                  output reg y1,
+                                  output reg y2,
+                                  output reg y3);
+                        
+  always @(*) begin
+    assign y1=a >= b;
+    assign y2= a <= b;
+    assign y3=a>b;
+  end
+endmodule
 
