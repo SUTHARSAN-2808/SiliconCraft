8d2375ce74cadfb574eca30ce8039c9cabcba1ac
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-03-01 07:14:27 +0530

    Update oddparity.v

--- Verilog/Day8/Day9/PARITY_GENERATOR/oddparity.v
+++ Verilog/Day8/Day9/PARITY_GENERATOR/oddparity.v
@@ -1,1 +1,8 @@
-
+module odd_parity_generator(input a,
+                       input b,
+                        input c,
+                       output z);
+wire d;
+  xnor xnor1(d,c,b);
+  xor xor1(z,a,d);
+endmodule
