6cbc33e1be10664f46a1c0f51184b7f0e9d23a61
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-03-01 07:17:24 +0530

    Delete Verilog/Day8/Day9/PARITY_GENERATOR directory

--- Verilog/Day8/Day9/PARITY_GENERATOR/oddparity.v
+++
@@ -1,8 +0,0 @@
-module odd_parity_generator(input a,
-                       input b,
-                        input c,
-                       output z);
-wire d;
-  xnor xnor1(d,c,b);
-  xor xor1(z,a,d);
-endmodule
