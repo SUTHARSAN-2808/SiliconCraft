9a5852b409c9295023ecc62ba1729932e3a26bd6
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-03-01 21:51:49 +0530

    Update random_number.v

--- Verilog/Day9/RANDOM_NUMBER/random_number.v
+++ Verilog/Day9/RANDOM_NUMBER/random_number.v
@@ -1,1 +1,18 @@
-
+module random_no(input [2:0]a);
+  
+  
+  task rand(input [2:0]x);    
+  if(x%2!=0)
+    $display("The number is odd=%d",$random);
+    else
+      $display("Enter some other number=%d",$random);
+  endtask
+    initial begin
+      reg[2:0] random_value;
+      random_value=$random_range(0,99);
+     
+      rand(random_value);
+    end
+ endmodule
+      
+   
