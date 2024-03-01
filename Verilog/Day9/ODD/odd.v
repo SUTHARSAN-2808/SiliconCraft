c19684fb1757ebb43be6016bcbb0aa2102c2e763
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-03-01 21:50:23 +0530

    Update odd.v

--- Verilog/Day9/ODD/odd.v
+++ Verilog/Day9/ODD/odd.v
@@ -1,1 +1,16 @@
+module odd_no(input a);
+  
+task odd(input x);    
+  if(x%2!=0)
+    $display("The number is odd");
+    else
+      $display("Enter some other number");
+  endtask
+    initial begin
+      odd(6);
+      odd(9);
+    end
+ endmodule
+      
+   
 
