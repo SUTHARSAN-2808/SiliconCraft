6570a5f0bbe5ead721c90f0dc16b3877d6121f09
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-03-01 21:48:48 +0530

    Update even.v

--- Verilog/Day9/EVEN/even.v
+++ Verilog/Day9/EVEN/even.v
@@ -1,1 +1,17 @@
+module even_no(input a);
+  
+task even(input x);    
+    if(x%2==0)
+      $display("The number is even");
+    else
+      $display("Enter some other number");
+  endtask
+    initial begin
+      even(6);
+      even(9);
+    end
+ endmodule
+      
+   
 
+     
