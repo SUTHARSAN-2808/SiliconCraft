810415cfc0a59d59e59e25dbf97916c5a2331f3c
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-03-01 07:24:15 +0530

    Update odd_parity_tb.v

--- Verilog/Day9/PARITY_GENERATOR/ODD_PARITY/odd_parity_tb.v
+++ Verilog/Day9/PARITY_GENERATOR/ODD_PARITY/odd_parity_tb.v
@@ -1,1 +1,19 @@
-
+module oddparity_tb;
+  reg a,b,c;
+  wire z;
+//instantiate
+  odd_parity_generator k(a,b,c,z);
+//Dumping
+  initial begin
+    $dumpfile("odd_parity_generator.vcd");
+    $dumpvars(1);
+  end
+//Entering the value
+  initial begin
+    repeat (6) begin
+      a=$random ;b=$random ;c=$random;#5
+      $display(a,b,c,z);
+    end
+      $finish;
+  end
+endmodule
