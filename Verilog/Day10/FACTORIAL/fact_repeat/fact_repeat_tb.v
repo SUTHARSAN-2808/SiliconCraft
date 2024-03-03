af6f36a617a417cf1bb4bc26a0ad6272128bcfb1
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-03-03 13:28:12 +0530

    Update fact_repeat_tb.v

--- Verilog/Day10/FACTORIAL/fact_repeat/fact_repeat_tb.v
+++ Verilog/Day10/FACTORIAL/fact_repeat/fact_repeat_tb.v
@@ -1,1 +1,18 @@
-
+module factorial_tb;
+  reg [3:0]a;
+  wire [20:0]fact;
+  factorial_repeat b(a,fact);
+  initial begin
+    $dumpfile("factorial.vcd");
+    $dumpvars(1);
+   end
+  initial begin
+    repeat (5)begin
+  
+      #4 a=$random;
+      $display("a=%d ",a);
+    end
+      #35 $finish;
+    
+  end
+endmodule
