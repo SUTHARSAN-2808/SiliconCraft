c294e1fab57b2795ab252671d357147320ec5bd0
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-03-03 21:59:34 +0530

    Update shift_reg_rand_tb.v

--- Verilog/Day10/SHIFT_REGISTER/shift_register_rand/shift_reg_rand_tb.v
+++ Verilog/Day10/SHIFT_REGISTER/shift_register_rand/shift_reg_rand_tb.v
@@ -1,1 +1,21 @@
-
+module shiftreg_rand_tb;
+  reg clk,reset;
+  reg [3:0] d;
+  wire [3:0] q;
+  shiftreg_rand dd(clk,reset,d,q);
+  initial begin
+    $dumpfile("shiftreg_rand.vcd");
+    $dumpvars(1);
+  end
+  initial begin
+    clk=1;
+    reset=0;
+    d=$random; 
+     #40 $finish;
+  end
+ 
+    always #5 clk=~clk;
+    always #10 reset=~reset;
+  
+endmodule
+    
