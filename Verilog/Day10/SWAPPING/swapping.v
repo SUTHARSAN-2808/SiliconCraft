a300f3e6e39c0814fbbd7a8c8579f24320e8c796
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-03-03 13:58:40 +0530

    Update swapping.v

--- Verilog/Day10/SWAPPING/swapping.v
+++ Verilog/Day10/SWAPPING/swapping.v
@@ -1,1 +1,11 @@
+module swapping(input  [4:0]a,input  [4:0]b,output reg [4:0]out_a,output reg [4:0]out_b);
+  
+  always @(*) begin
+    
+    out_a=b;
+    out_b=a;
+    $display("a=%d b=%d",out_a,out_b);
+  end
+endmodule
+  
 
