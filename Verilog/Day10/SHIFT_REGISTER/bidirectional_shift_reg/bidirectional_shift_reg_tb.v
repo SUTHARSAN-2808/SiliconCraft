46057bb03a76c0e7561d14da2a213256284a3d7b
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-03-03 22:01:24 +0530

    Update bidirectional_shift_reg_tb.v

--- Verilog/Day10/SHIFT_REGISTER/bidirectional_shift_reg/bidirectional_shift_reg_tb.v
+++ Verilog/Day10/SHIFT_REGISTER/bidirectional_shift_reg/bidirectional_shift_reg_tb.v
@@ -1,1 +1,25 @@
-
+module bidirectional_shiftreg_tb;
+  reg clk,reset;
+  reg [3:0] d;
+  reg [1:0]s;
+  wire [3:0] q;
+  bidirectional_shiftreg dd(.clk(clk),.reset(reset),.d(d),.s(s),.q(q));
+  initial begin
+    $dumpfile("bidirectional_shiftreg.vcd");
+    $dumpvars(1);
+  end
+  initial begin
+    clk=1;
+    reset=0;
+    s=1;
+    #5 d=0100; 
+     $display("d=%b q=%b",q,d);
+     #40 $finish;
+  end
+ 
+    always #5 clk=~clk;
+    always #5 s=~s;
+    always #10 reset=~reset;
+  
+endmodule
+    
