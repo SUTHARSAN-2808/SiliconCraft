32c7ece2bebafff3e7ce0d665087d41d5c9824ae
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-03-03 21:53:54 +0530

    Update shift_register.v

--- Verilog/Day10/SHIFT_REGISTER/shift_register/shift_register.v
+++ Verilog/Day10/SHIFT_REGISTER/shift_register/shift_register.v
@@ -1,1 +1,8 @@
-
+module shift_registers;
+  initial begin
+    $display("4'b0101 << 1=%b",(4'b0101<<1));
+    $display("4'b0111 << 1=%b",(4'b0111<<1));
+    $display("4'b0101 << 1=%b",(4'b0101 >>1));
+    
+  end
+endmodule
