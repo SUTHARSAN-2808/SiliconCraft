4e925a72a94ce2540bf1ae3ea169f13032b492db
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-03-03 13:23:45 +0530

    Update fact_task.v

--- Verilog/Day10/FACTORIAL/fact_task/fact_task.v
+++ Verilog/Day10/FACTORIAL/fact_task/fact_task.v
@@ -1,1 +1,18 @@
+module factorial_task(input [3:0]a,output reg[15:0] factor);
+  initial begin
+    factor=1;
+    fact(5,factor);
+  end
 
+  task fact(input[3:0] x,output [15:0] b);begin
+      b=1;
+      for(int i=x;i>0;i=i-1)begin
+      b=b*i;
+      end
+    
+      $display("the factorialnumber is=%d",b);
+     end
+    endtask
+  
+endmodule
+    
