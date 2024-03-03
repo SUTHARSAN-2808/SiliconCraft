bed69ce67627752cc94730f1dbee94ec177d80c2
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-03-03 22:19:24 +0530

    Update clock_generator.v

--- Verilog/Day10/CLOCK_GENERATOR/clock_generator.v
+++ Verilog/Day10/CLOCK_GENERATOR/clock_generator.v
@@ -1,1 +1,14 @@
-
+module clk_generator(output reg clk);
+  initial begin
+    clk=0;
+    #2
+    clk=1;
+    #4
+    clk=0;
+  
+    #20 $finish;
+  end
+  always @(*) begin
+    $display(clk);
+  end
+endmodule
