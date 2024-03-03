1ed1a2158b6e872216fdb7adea5ea27c3f617fb1
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-03-03 22:00:47 +0530

    Update bidirectional_shift_reg.v

--- Verilog/Day10/SHIFT_REGISTER/bidirectional_shift_reg/bidirectional_shift_reg.v
+++ Verilog/Day10/SHIFT_REGISTER/bidirectional_shift_reg/bidirectional_shift_reg.v
@@ -1,1 +1,19 @@
-
+module bidirectional_shiftreg(input reg clk,input reg reset,input reg[3:0] d,output reg[3:0]q,input reg[1:0]s);
+  
+  always @(*)begin
+    if (reset)
+      q=d;
+    else begin
+    case(s) 
+      1'b0: q=d>>1;//Shift right operation
+     
+      1'b1: q=d<<1;//Shift left operation
+      
+    endcase
+    end   
+  end
+endmodule
+    
+    
+    
+      
