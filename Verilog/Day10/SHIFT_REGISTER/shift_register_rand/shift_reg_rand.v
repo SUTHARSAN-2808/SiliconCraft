5f0c2530004dc01e2e53face2239c9b705c3e369
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-03-03 21:59:16 +0530

    Update shift_reg_rand.v

--- Verilog/Day10/SHIFT_REGISTER/shift_register_rand/shift_reg_rand.v
+++ Verilog/Day10/SHIFT_REGISTER/shift_register_rand/shift_reg_rand.v
@@ -1,1 +1,15 @@
-
+module shiftreg_rand(input reg clk,input reg reset,input reg [3:0]d,output reg [3:0]q);
+ 
+  always @(posedge clk) begin
+    if(reset)begin
+      q=d;
+      $display("q=%b d=%b",q,d);
+    end
+    else begin
+      q=d<<1;
+      $display("q=%b d=%b",q,d);
+    end
+  end
+  
+endmodule
+  
