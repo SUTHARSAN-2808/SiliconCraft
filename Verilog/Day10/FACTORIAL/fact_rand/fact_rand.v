2c5fad64c6b01f7fa0acc560db8a9bc957e142d7
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-03-03 13:29:28 +0530

    Update fact_rand.v

--- Verilog/Day10/FACTORIAL/fact_rand/fact_rand.v
+++ Verilog/Day10/FACTORIAL/fact_rand/fact_rand.v
@@ -1,1 +1,19 @@
+module factorial(input reg[3:0]a,output reg [20:0]fact);
 
+ integer i;
+  
+  
+  always @(*) begin
+  fact=1;
+   
+    for( i=a;i>0;i=i-1)begin
+      
+      fact=fact*i;
+    
+    $display("The factorial value is=%d",fact);
+    end
+    end
+ 
+  
+endmodule
+  
