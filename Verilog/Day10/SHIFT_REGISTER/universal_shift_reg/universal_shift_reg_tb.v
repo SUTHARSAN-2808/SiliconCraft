69616337a6a77ed212ad1ccd1aaa664c9d765daf
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-03-03 22:03:54 +0530

    Update universal_shift_reg_tb.v

--- Verilog/Day10/SHIFT_REGISTER/universal_shift_reg/universal_shift_reg_tb.v
+++ Verilog/Day10/SHIFT_REGISTER/universal_shift_reg/universal_shift_reg_tb.v
@@ -1,1 +1,24 @@
-
+module universal_shiftreg_tb;
+  reg clk,reset;
+  reg [3:0] d;
+  reg [1:0]s;
+  wire [3:0]q;
+  universal_shiftreg aa(.clk(clk),.reset(reset),.d(d),.s(s),.q(q));
+  initial begin
+    $dumpfile("universal_shiftreg.vcd");
+    $dumpvars(1);
+  end
+  initial begin
+    clk=1;
+    reset=0;
+    #3 s=00;d=1001;
+    #4 s=01;
+    #5 s=11;
+    #3 s=10;
+    $display(s,d,q);
+  
+    #46$finish;
+  end
+endmodule
+  
+    
