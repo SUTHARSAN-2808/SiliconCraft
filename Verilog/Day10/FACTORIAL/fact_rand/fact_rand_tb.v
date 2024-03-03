e8ba9fff1bde8fa0cbca36496161c3cfad8cf887
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-03-03 13:29:47 +0530

    Update fact_rand_tb.v

--- Verilog/Day10/FACTORIAL/fact_rand/fact_rand_tb.v
+++ Verilog/Day10/FACTORIAL/fact_rand/fact_rand_tb.v
@@ -1,1 +1,18 @@
+module factorial_tb;
+  reg [3:0]a;
+  wire [20:0]fact;
+  factorial b(a,fact);
+  initial begin
+    $dumpfile("factorial.vcd");
+    $dumpvars(1);
+   end
+  initial begin
+  
+      #4 a=$random;
+      $display("a=%d ",a);
+    
+      #35 $finish;
+    
+  end
+endmodule
 
