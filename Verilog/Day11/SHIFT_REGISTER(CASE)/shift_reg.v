560e250acfff2dc1d155711f351059b61c79f728
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-03-05 16:23:57 +0530

    Update shift_reg.v

--- Verilog/Day11/SHIFT_REGISTER(CASE)/shift_reg.v
+++ Verilog/Day11/SHIFT_REGISTER(CASE)/shift_reg.v
@@ -1,1 +1,27 @@
+module shift_reg(
+  input  reg clk,
+  input reset,
+  input reg [3:0]d,
+  input din,
+  input reg [1:0] s,
+  output reg [3:0] q
+);
 
+    always @(posedge clk) begin
+      if(reset) 
+        q<=4'b0000;
+      
+      else
+        
+        case(s) 
+        1'b0: q<={din,d[3:1]};
+          
+          1'b1: q<={d[2:0],din};
+        
+       endcase
+      
+    end
+  
+  always @(*)
+    $display("d=%b din=%b s=%b q=%b",d,s,din,q);   
+endmodule
