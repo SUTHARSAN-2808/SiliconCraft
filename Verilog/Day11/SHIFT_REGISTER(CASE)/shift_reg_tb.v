cd59608f3d556ee3f800550baa5d8b85f5a48290
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-03-05 16:24:42 +0530

    Update shift_reg_tb.v

--- Verilog/Day11/SHIFT_REGISTER(CASE)/shift_reg_tb.v
+++ Verilog/Day11/SHIFT_REGISTER(CASE)/shift_reg_tb.v
@@ -1,1 +1,20 @@
-
+module shift_reg_tb;
+  reg clk,reset,din;
+  reg [1:0] s;
+  reg [3:0] d;
+  wire [3:0]q;
+  shift_reg aa(.clk(clk),.reset(reset),.din(din),.s(s),.d(d),.q(q));
+  
+  
+  initial begin
+    clk=1;
+     reset=0;
+      s=0;
+     din=1;
+    d=1101;
+     #40 $finish;
+  end
+   
+    always #10 clk = ~clk;  
+    always #10 reset = ~reset; 
+endmodule
