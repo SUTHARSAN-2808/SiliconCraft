4ffca7d38f43d8f6732deb09adfa53d383d3dba9
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-03-05 16:42:25 +0530

    Update up_down_counter.v

--- Verilog/Day11/UP_DOWN_COUNTER/up_down_counter.v
+++ Verilog/Day11/UP_DOWN_COUNTER/up_down_counter.v
@@ -1,1 +1,20 @@
-
+module UpDowncounter(input reg clk,
+               input reg reset, 
+               input reg[1:0] s,
+               output reg[3:0]count);
+  always @(posedge clk )begin
+    if(reset)
+      count <=4'b0000;
+    else  
+      case(s)
+      1'b0:count <= count-1;//Down_counter   
+      1'b1:count <= count+1;//Up_counter
+      endcase
+  end
+    
+       
+  always @(*) begin
+    $monitor(" clk=%d s=%d count=%d",clk,s,count );
+  end
+endmodule
+       
