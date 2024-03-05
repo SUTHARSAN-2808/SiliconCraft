e09c9704c26569583708067caf8b70a9313822c5
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-03-05 22:38:02 +0530

    Update bcd_counter.v

--- Verilog/Day11/BCD_counter/bcd_counter.v
+++ Verilog/Day11/BCD_counter/bcd_counter.v
@@ -1,1 +1,16 @@
-
+module bcd_counter(input reg clk,
+                  input  reset,                 
+                   output reg [3:0]cout);
+  always @(posedge clk ) begin
+    if(reset)
+      cout<=4'b0000;
+    else if (cout==4'b1010)
+      cout<=4'b0000;
+    else
+      cout<=cout+1;
+  end
+initial begin
+  $monitor("clk=%b reset=%b cout=%b",clk,reset,cout);
+  end
+ 
+endmodule
