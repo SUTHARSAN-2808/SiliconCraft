997ee090a8be29ba076e1273c9cb1f096ed61541
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-03-05 22:38:20 +0530

    Update bcd_counter_tb.v

--- Verilog/Day11/BCD_counter/bcd_counter_tb.v
+++ Verilog/Day11/BCD_counter/bcd_counter_tb.v
@@ -1,1 +1,15 @@
-
+module bcd_counter_tb;
+  reg clk,reset;
+  wire [3:0]cout;
+  bcd_counter aa(.clk(clk),.reset(reset),.cout(cout));
+   always #5 clk=~clk;
+  initial begin
+    clk=0;  
+    reset=1;
+    #50 reset=0;
+    #200 $finish;   
+  end
+    
+endmodule
+    
+  
