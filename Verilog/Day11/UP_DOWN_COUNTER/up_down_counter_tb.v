5efc1763a678b3ebcef291ce75a65e825de0c337
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-03-05 16:43:04 +0530

    Update up_down_counter_tb.v

--- Verilog/Day11/UP_DOWN_COUNTER/up_down_counter_tb.v
+++ Verilog/Day11/UP_DOWN_COUNTER/up_down_counter_tb.v
@@ -1,1 +1,24 @@
-
+module UpDowncounter_tb;
+  reg clk,reset;
+  reg [1:0]s;
+  wire [3:0] count;
+  UpDowncounter aa(.clk(clk),
+                   .s(s),
+               .reset(reset),
+               .count(count));
+  
+  always #5 clk=~clk;
+   initial begin
+    clk=0;
+    reset=1;
+     
+   #10
+     reset = 0;
+     s=0;
+     #5 s=1;
+     #5 s=0;
+     #250
+     $finish;
+   end
+    
+endmodule
