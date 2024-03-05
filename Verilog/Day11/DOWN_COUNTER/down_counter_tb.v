8587cdb0ad5ae2ecf4d219b28e6aa2026024265b
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-03-05 16:28:21 +0530

    Update down_counter_tb.v

--- Verilog/Day11/DOWN_COUNTER/down_counter_tb.v
+++ Verilog/Day11/DOWN_COUNTER/down_counter_tb.v
@@ -1,1 +1,20 @@
-
+module Downcounter_tb;
+  reg clk,reset;
+  wire [3:0] count;
+  Downcounter aa(.clk(clk),
+               .reset(reset),
+               .count(count));
+  
+  always #5 clk=~clk;
+   initial begin
+    clk=0;
+    reset=1;
+   #10
+     reset = 0;
+     #250
+     $finish;
+   end
+    
+endmodule
+  
+  
