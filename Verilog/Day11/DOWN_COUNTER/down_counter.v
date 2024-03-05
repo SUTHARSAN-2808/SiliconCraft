3d301330c5fee915d91ee2602bf762f8a5913d6d
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-03-05 16:27:45 +0530

    Update down_counter.v

--- Verilog/Day11/DOWN_COUNTER/down_counter.v
+++ Verilog/Day11/DOWN_COUNTER/down_counter.v
@@ -1,1 +1,15 @@
-
+module Downcounter(input reg clk,
+               input reg reset,              
+               output reg[3:0]count);
+  always @(posedge clk )begin
+    if(reset)
+      count <=4'b0000;
+    else 
+      count <= count-1;//Down_counter   
+  end
+  always @(*) begin
+    $monitor(" count=%d",count );
+  end
+endmodule
+       
+      
