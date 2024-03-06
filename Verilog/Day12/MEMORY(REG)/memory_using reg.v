172386862ff505d8622c3c4c3a2269a418ca3a43
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-03-06 14:30:20 +0530

    Update memory_using reg.v

--- Verilog/Day12/MEMORY(REG)/memory_using reg.v
+++ Verilog/Day12/MEMORY(REG)/memory_using reg.v
@@ -1,1 +1,16 @@
-
+module memory(input wr,
+              input clk,            
+              input [4:0]data,             
+              input reset,
+              output [4:0]rd_data);
+  reg [7:0] register;
+  always @(posedge clk)begin
+    if(reset)
+      register<=0;
+    else if( wr)
+      register<=data;
+  end
+  assign 
+    rd_data =(~wr)? register:0;
+ 
+endmodule
