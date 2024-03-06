d712a18c4c114b4a703997f3a7b2b9fb558c804c
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-03-06 07:15:49 +0530

    Update ring_counter_tb.v

--- Verilog/Day12/RING_COUNTER/ring_counter_tb.v
+++ Verilog/Day12/RING_COUNTER/ring_counter_tb.v
@@ -1,1 +1,21 @@
+module ring_counter_tb;
+parameter width=4;
+  reg clk,reset;
+  wire [3:0] cout;
 
+  ring_counter aa (
+    .clk(clk),
+    .reset(reset),
+    .cout(cout)
+  );
+
+  always #5 clk = ~clk;
+
+  initial begin
+    clk = 0;
+    reset = 1;
+    #10 reset = 0;
+    #100 $finish;
+  end
+
+endmodule
