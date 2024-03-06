7e35e8b8fe1eb2494e695166a7fbab0b869a992b
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-03-06 14:31:01 +0530

    Update memory_using_reg_tb.v

--- Verilog/Day12/MEMORY(REG)/memory_using_reg_tb.v
+++ Verilog/Day12/MEMORY(REG)/memory_using_reg_tb.v
@@ -1,1 +1,17 @@
-
+module memory_tb;
+  reg clk,reset,wr;
+  reg [4:0]data;
+ 
+  wire [4:0]rd_data;
+  memory aa(.clk(clk),.reset(reset),.wr(wr),.data(data),.rd_data(rd_data));
+  always #10 clk=~clk;
+  initial begin
+    clk=1;reset=0;wr=1;data=1100;
+    #30 clk=1;reset=0;wr=0;
+  #65  $finish;
+  end
+  initial begin
+    $monitor("clk=%d wr=%d reset=%d rdata=%b",clk,wr,reset,rd_data);
+  end
+endmodule
+    
