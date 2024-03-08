5b91ad2804cd40c76bd5a2844722cf0c7f8992ba
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-03-08 23:22:38 +0530

    Update and rename fsm(1010). v to fsm(1010).v

--- Verilog/Day13/FSM/fsm(1010).v
+++ Verilog/Day13/FSM/fsm(1010).v
@@ -1,14 +1,13 @@
-
 module fsm3(input reg clk,
            input reset,
            input reg in,
             output  state,next,
             output reg out);
   parameter  [2:0]s0=3'b000,
-  				  s1=3'b001,
-     			  s2=3'b010,
-                  s3=3'b011,
-  			      s4=3'b100;
+                   s1=3'b001,
+                    s2=3'b010,
+                   s3=3'b011,
+  	        s4=3'b100;
   reg [2:0] state,next;
   always @(posedge clk)begin
     if(reset)
