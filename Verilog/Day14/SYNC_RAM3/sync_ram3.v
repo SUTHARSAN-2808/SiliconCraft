fd49ab029c3bc85055a1d7b48443ce5e13b5685e
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-03-09 23:14:40 +0530

    Update sync_ram3.v

--- Verilog/Day14/SYNC_RAM3/sync_ram3.v
+++ Verilog/Day14/SYNC_RAM3/sync_ram3.v
@@ -1,1 +1,17 @@
-
+module sync_ram3(input reg clk,
+                input reset,
+                 input reg[4:0]datain,
+                 input reg[7:0]addr,
+                input wr,
+                input rd,
+                 output reg[4:0]dataout);
+  reg[4:0]sram[256:0];
+  always @(posedge clk)begin
+    if(reset)
+      dataout<=0;
+    else if(wr==1)
+      sram[addr]<=datain;
+    else if (rd==1)
+      dataout<=sram[addr];
+  end
+endmodule
