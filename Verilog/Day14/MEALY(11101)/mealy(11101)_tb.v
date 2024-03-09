570d990a672546235c9d85f7788b719a2d61682d
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-03-09 22:15:10 +0530

    Update mealy(11101)_tb.v

--- Verilog/Day14/MEALY(11101)/mealy(11101)_tb.v
+++ Verilog/Day14/MEALY(11101)/mealy(11101)_tb.v
@@ -1,1 +1,30 @@
-
+module mealy_tb;
+  reg clk,reset;
+  reg in;
+  wire [2:0] state,next;
+  wire out;
+  mealy ab(.clk(clk),.reset(reset),.state(state),.next(next),.in(in),.out(out));
+  always #2clk=~clk;
+  initial begin
+    clk=0;
+    reset=1;
+    #4 reset=0;
+  end
+  initial begin
+    #5 in=1'b1;
+    #5 in=1'b1;
+    #5 in=1'b1;
+    #5 in=1'b0;
+    #5 in=1'b1; 
+    #15$finish;
+  end
+  initial begin
+    $monitor("clk=%d reset=%d in=%b state=%b next=%b out=%d",clk,reset,in,state,next,out);
+  end
+  initial begin
+    $dumpfile("mealy.vcd");
+    $dumpvars(1);
+  end
+endmodule
+  
+    
