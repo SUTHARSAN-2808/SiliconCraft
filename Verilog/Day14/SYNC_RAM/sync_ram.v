d57888328859f01384fd3093dfd57328d23b4a77
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-03-09 21:51:22 +0530

    Update sync_ram.v

--- Verilog/Day14/SYNC_RAM/sync_ram.v
+++ Verilog/Day14/SYNC_RAM/sync_ram.v
@@ -1,1 +1,28 @@
-
+module sync_ram(datain,dataout,clk,reset,addr,wr,rd);
+  
+ parameter DATA=8;
+  parameter ADDR=8;
+  
+  input reg[DATA-1:0]datain;
+  output reg [DATA-1:0]dataout;
+  input reg[ADDR-1:0]addr;
+  input reg clk,wr,rd,reset;
+  
+  reg[7:0]ram[256:0];
+  
+  always @(posedge clk)begin
+    
+    if(reset)begin
+      dataout=0;
+    end
+    else if((clk) && (wr))begin
+      ram[addr]=datain;
+    end
+    else if((clk)  && (rd))begin
+      dataout=ram[addr];
+    end
+  end
+endmodule
+    
+  
+  
