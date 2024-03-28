0bdd6987603b4f46b053ca0604074e25bd4ce6a4
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-03-28 14:54:40 +0530

    Update dual_port_ram2.v

--- Verilog/Day15/DUAL_PORT_RAM2/dual_port_ram2.v
+++ Verilog/Day15/DUAL_PORT_RAM2/dual_port_ram2.v
@@ -1,1 +1,26 @@
-
+module dualport_ram2(input reg clk,
+                     input reset,
+                     input reg[4:0]datain,
+                     input port,
+                     input wr,
+                     input rd,
+                     input [5:0]addr0,
+                     input [5:0]addr1,
+                     output reg[4:0]dataout0,
+                     output reg[4:0]dataout1);
+  reg[4:0]mem[64:0];
+  always @(posedge clk)begin
+    if(reset)begin
+      dataout0<=0;
+      dataout1<=0;
+    end
+    else if(wr==1 && port==0)
+      mem[addr0]<=datain;
+    else if(wr==1 && port==1)
+      mem[addr1]<=datain;
+    else if(rd==1 && port==0)
+      dataout0<=mem[addr0];
+    else if(rd==1 && port==1)
+      dataout1<=mem[addr1];
+  end
+endmodule
