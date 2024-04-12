ce7d7a27e908917a0a344c65fa0532d6c2e2bbdd
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-04-12 23:57:30 +0530

    Update Associative_array.sv

--- Systemverilog/Associative_array.sv
+++ Systemverilog/Associative_array.sv
@@ -1,1 +1,24 @@
+module associative_array;
 
+  typedef enum {red,yellow,green} array;
+  bit[5:0] a[array];
+  bit[5:0] b[int];
+
+  initial begin
+    a[red]=5;
+    a[yellow]=7;
+    a[green]=9;
+    foreach(a[i])
+      $display("a[%s]=%0d",i.name(),a[i]);
+  end
+  initial begin
+    b[12]=50;
+    b[15]=70;
+    b[18]=90;
+    foreach(b[i])
+      begin
+        $display("b[%0d]=%0d",i,b[i]);
+      end
+  end
+endmodule
+  
