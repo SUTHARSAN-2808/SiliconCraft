76ba81b59288c20660bed483ad67feac9888af9e
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-04-12 23:56:04 +0530

    Update Queue.sv

--- Systemverilog/Queue.sv
+++ Systemverilog/Queue.sv
@@ -1,1 +1,20 @@
-
+module queue;
+  int a[$] = '{2,3,4,6,8};
+  int b;
+  
+  initial begin
+    b=2; 
+    $display(b);
+  a.push_front(5);
+  $display(a);
+    a.insert(1,9);
+    $display(a);
+    a.delete(3);
+      $display(a);
+   a.push_front(7);
+    $display(a);
+  a.pop_back();
+      $display(a);
+  end
+  
+endmodule
