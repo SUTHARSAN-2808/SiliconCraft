091ddab6986e2b78676082d009dbd798f93802b7
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-04-13 00:02:00 +0530

    Update 3d_packed_array.sv

--- Systemverilog/3d_packed_array.sv
+++ Systemverilog/3d_packed_array.sv
@@ -1,1 +1,16 @@
-
+module three_dimensional_packed_array;
+  logic [1:0][3:0][5:0]a;
+  initial begin
+    a[1]=36'hface48;
+    a[0]=36'h1a9764;
+    foreach(a[i])
+      $display("a[%h]=%h",i,a[i]);
+    a=48'hface02_cafe09;
+    $display("a=%h",a);
+    foreach(a[i])begin
+      $display("a[%d]=%h",i,a[i]);
+    foreach(a[i][j])
+      $display("a[%d][%d]=%h",i,j,a[i][j]);
+    end
+  end
+endmodule
