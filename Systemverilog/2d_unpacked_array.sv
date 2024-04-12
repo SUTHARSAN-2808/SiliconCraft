268ac73cf63e8a80860e6369c6382ffc13c26fa2
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-04-13 00:00:43 +0530

    Update 2d_unpacked_array.sv

--- Systemverilog/2d_unpacked_array.sv
+++ Systemverilog/2d_unpacked_array.sv
@@ -1,1 +1,15 @@
+module two_dimensional_unpacked_array;
+  
+  bit a[3:0][3:0];
+
+  initial begin
+    a[3]=4'ha;
+    a[2]=4'h1;
+    a[1]=4'hf;
+    a[0]=4'h8;
+    foreach(a[i])
+      $display("a[%d]=%h",i,a[i]);
+  end
+endmodule
+  
 
