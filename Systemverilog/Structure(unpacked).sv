469a2ff9d5d29e01d1833b2ce57056ce76aae59c
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-04-15 20:04:19 +0530

    Update Structure(unpacked).sv

--- Systemverilog/Structure(unpacked).sv
+++ Systemverilog/Structure(unpacked).sv
@@ -1,1 +1,18 @@
-
+module structure;     //unpacked structure
+  typedef struct {
+    int a;
+    reg [3:0] b ;
+  } practice;
+  
+  initial begin
+    practice b1,b2;
+    b1.a='hfa235_1;
+    b1.b='h2;
+    $display("b1=%0h",b1.a);
+    $display("b1=%p",b1.b);
+    
+    b2='{'hfcae233,'h2};
+ 
+    $display("b2=%p",b2);
+  end
+endmodule
