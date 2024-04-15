7abad7fe5ab1daa829e75e838ddd3070ff797b7a
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-04-15 20:02:04 +0530

    Update Union.sv

--- Systemverilog/Union.sv
+++ Systemverilog/Union.sv
@@ -1,1 +1,16 @@
+module union_a;
+  
+  typedef union{
+    int a;
+    logic [7:0] b;
+  }students;
+  initial begin
+    students akash;
+    
+    akash.a='h86108570;
+   // akash.b='h32;
+    $display("akash=%p",akash);
+  end
+  
+endmodule
 
