719876593de7dab84e3a7b9e428b45b05fb5e2d0
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-04-17 20:03:42 +0530

    Update foreach_loop.sv

--- Systemverilog/foreach_loop.sv
+++ Systemverilog/foreach_loop.sv
@@ -1,1 +1,13 @@
-
+module foreach_loop;
+  bit [2:0][3:0] aa;
+  
+  initial begin
+    aa='habc;
+    $display("aa=%h",aa);
+    foreach(aa[i])begin
+      $display("aa[%d]=%h",i,aa[i]);
+      foreach(aa[i][j])
+        $display("aa[%d][%d]=%d",i,j,aa[i][j]);
+    end
+  end
+endmodule
