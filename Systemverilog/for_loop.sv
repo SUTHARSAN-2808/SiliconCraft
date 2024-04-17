7dd5a7d9e50ee5d00b12734e1e308ed9df2b40b2
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-04-17 19:46:37 +0530

    Update for_loop.sv

--- Systemverilog/for_loop.sv
+++ Systemverilog/for_loop.sv
@@ -1,1 +1,11 @@
-
+module for_loop;
+  int mem[0:2];
+  initial begin
+    for(int i=0;i<3;i++)begin
+      for(int j=0;j<4;j++)begin
+        mem[i]=j+3;
+        $display("mem[%d][%d]=%d",i,j,mem[i]);
+      end
+    end
+  end
+endmodule
