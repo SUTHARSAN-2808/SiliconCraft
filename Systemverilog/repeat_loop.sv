edc6ed2761d8f3914b7dcf38c9e2b9af5e31fcae
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-04-24 15:43:17 +0530

    Update repeat_loop.sv

--- Systemverilog/repeat_loop.sv
+++ Systemverilog/repeat_loop.sv
@@ -1,1 +1,12 @@
-
+module repeat_loop;
+  int arr[3]='{2,5,8};
+  int i;
+  initial begin
+    repeat ($size(arr))begin
+      #5 $monitor("arr[%0d]=%0d",i,arr[i]);
+    i++;
+    end
+    repeat (5)
+      $display("Silicon craft");
+  end
+endmodule
