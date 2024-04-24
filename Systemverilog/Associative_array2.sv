73b2a75aa9328396c296e8b783e3ba7bcad87e4d
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-04-24 22:01:45 +0530

    Update Associative_array2.sv

--- Systemverilog/Associative_array2.sv
+++ Systemverilog/Associative_array2.sv
@@ -1,1 +1,20 @@
-
+module sparse_array;
+  longint arra[int];
+  int idx;
+  initial begin
+    idx=1;
+    repeat(64)begin
+      arra[idx]=idx;
+      idx=idx<<1;
+    end
+    if(arra.first(idx))
+      $display(idx," ",arra[idx]);
+    if(arra.next(idx))
+      $display(idx," ",arra[idx]);
+    if(arra.last(idx))
+      $display(idx," ",arra[idx]); 
+    if(arra.prev(idx))
+      $display(idx," ",arra[idx]);
+  end
+endmodule
+      
