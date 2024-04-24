60c07205f6a73f954ead8c06b9592b9773c89ece
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-04-24 15:42:16 +0530

    Update forever_loop.sv

--- Systemverilog/forever_loop.sv
+++ Systemverilog/forever_loop.sv
@@ -1,1 +1,15 @@
+module forever_loop;
+  bit clk;
 
+  initial begin
+    clk=0;
+  
+  forever begin
+    #5 clk=!clk;
+     $monitor("clk=%d",clk);
+  end
+  end
+  initial begin
+    #20 $finish;
+  end
+endmodule
