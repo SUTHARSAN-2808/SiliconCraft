dc6b67968610cd305c1408918191e4456fc38b3b
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-04-25 09:33:14 +0530

    Update String_methods2.sv

--- Systemverilog/String_methods2.sv
+++ Systemverilog/String_methods2.sv
@@ -1,1 +1,16 @@
-
+module string_methods;
+  string a="SystemVerilog";
+  string b,c;
+  initial begin
+    $display(a);
+    $display(a.getc(1));
+    $display(a.toupper);
+    b="3.1a";
+    c={a,b};
+    $display(c);
+    $display(c.len);
+    c.putc(c.len()-1,"b");
+    $display(c);
+    $display(c.substr(2,5));
+  end
+endmodule
