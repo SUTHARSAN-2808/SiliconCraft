e378b791bd840391ff96be9f9d6dbda4737673bd
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-05-01 22:04:35 +0530

    Update Class_routines.sv

--- Systemverilog/Class_routines.sv
+++ Systemverilog/Class_routines.sv
@@ -1,1 +1,26 @@
+//Class routines
+class students;
+  int marks,age;
+  function void display();
+    $display(marks,age);
+  endfunction
+endclass
 
+class emp;
+  int salary;
+  function void print();
+    $display(salary);
+  endfunction
+endclass
+
+module datas;
+  initial begin
+    students akash=new();
+    emp jai=new();
+    akash.marks=93;
+    akash.age=19;
+    jai.salary=20000;
+    akash.display();
+    jai.print();
+  end
+endmodule
