dc4d59c0d344ca20ddc4a83a274610c9b9394522
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-05-02 10:35:50 +0530

    Update Shallow_copy.sv

--- Systemverilog/Shallow_copy.sv
+++ Systemverilog/Shallow_copy.sv
@@ -1,1 +1,45 @@
+//Shallow copy
 
+class deepelement;
+  bit [8:0] start_addr;
+  bit [11:0] end_addr;
+  function new();
+    start_addr=56;
+    end_addr='hf4;
+  endfunction
+endclass
+
+class shallowelement;
+  bit[6:0] addr;
+  bit[12:0] data;
+  deepelement dp;
+  
+  function new();
+    addr=34;
+    data=23;
+    dp=new();
+  endfunction
+  
+  function void disp();
+    $display(addr);
+    $display(data);
+    $display(dp.start_addr);
+    $display(dp.end_addr);
+  endfunction
+endclass
+
+module shallowcopy;
+  initial begin
+    
+    shallowelement sh1,sh2;
+    sh1=new();
+    sh1.disp();
+    sh2=new sh1;
+    sh2.disp();
+    sh2.data=89;
+    sh2.addr=15;
+    sh2.dp.start_addr=44;
+    sh1.disp();
+    sh2.disp();
+  end
+endmodule
