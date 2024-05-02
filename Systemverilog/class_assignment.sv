1372337a9fdb66bba655d281e68f4ba10c1891ff
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-05-02 07:46:45 +0530

    Update class_assignment.sv

--- Systemverilog/class_assignment.sv
+++ Systemverilog/class_assignment.sv
@@ -1,1 +1,21 @@
+//Class Assignment
+class transaction;
+  bit [9:0]data;
+  function void display();
+    $display(data);
+  endfunction
+endclass
 
+module assignment;
+  initial begin
+    transaction good,bad;
+    good=new();
+    bad=good;
+    good.data=16;
+    good.display();
+    bad.display();
+    bad.data=11;
+    bad.display();
+    good.display();
+  end
+endmodule
