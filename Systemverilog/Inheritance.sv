981b593e565dc6691db2edb4d135d0f997d80022
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-05-02 21:52:36 +0530

    Update Inheritance.sv

--- Systemverilog/Inheritance.sv
+++ Systemverilog/Inheritance.sv
@@ -1,1 +1,27 @@
+class F;
+  bit[4:0] a,b,c;
+  function void display();
+    $display(a,",",b,",",c);
+  endfunction
+endclass
 
+class G extends F;
+  bit[2:0] d;
+  function void dissplay();
+    $display(a,",",b,",",c,",",d);
+  endfunction
+  
+endclass
+
+module inheritannce;
+   
+  initial begin
+    G one=new();
+    one.a=5'b10101;
+    one.b=5'b11111;
+    one.d=3'b000;
+    one.display;
+    one.dissplay;
+ 
+  end
+endmodule
