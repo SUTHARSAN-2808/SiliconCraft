ba42c3fbb71a63056b5cc66c685557196b297f78
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-05-02 21:40:51 +0530

    Update Parameterized classes.sv

--- Systemverilog/Parameterized classes.sv
+++ Systemverilog/Parameterized classes.sv
@@ -1,1 +1,17 @@
+//parameterized classes
+class god #(int size=8);
+  bit [size-1:0] out;
+endclass
 
+module good;
+  god #(.size(5)) bad;
+  god #(7) better;
+  
+  initial begin
+    bad=new();
+    better=new();
+    $display($bits(bad.out));
+    $display($bits(better.out));
+    
+  end
+endmodule
