58f0b7dd3182b9e8e5edca0a687ea50ca6c93e7e
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-05-02 22:26:06 +0530

    Update Inheritance2.sv

--- Systemverilog/Inheritance2.sv
+++ Systemverilog/Inheritance2.sv
@@ -1,1 +1,17 @@
+//Inheritance
+class parent;
+  bit[5:0] addr;
+ endclass
 
+class child extends parent;
+  bit [7:0] data;
+endclass
+
+  module inheritaces;
+    initial begin
+      child c=new();
+      c.data=10;
+      c.addr=5;
+      $display(c.data,",",c.addr);
+    end
+  endmodule
