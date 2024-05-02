7eee8a0673487bbe886dfa755e6d7524f24b406e
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-05-02 11:02:38 +0530

    Update Shallow_copy2.sv

--- Systemverilog/Shallow_copy2.sv
+++ Systemverilog/Shallow_copy2.sv
@@ -1,1 +1,43 @@
+class error_trans;
+  bit[5:0] error;
+  bit[6:0] data;
+  function new( bit[5:0] error,bit[6:0] data);
+    this.error=error;
+    this.data=data;
+  endfunction
+endclass
 
+class trans;
+  bit [31:0] a,b;
+  error_trans er;
+  function new();
+    a=55;
+    b=66;
+    er=new('hd1,324);
+  endfunction
+
+
+function void disp();
+  $display(a,",",b);
+  $display(er.error,",",er.data);
+  
+endfunction
+endclass
+
+module sh;
+  initial begin
+    trans t1,t2;
+    t1=new();
+    t1.disp();
+    t2= new t1;
+    t2.disp();
+    t2.a=36;
+    t2.b=49;
+    t2.er.error=10;
+    t2.er.data=20;
+    t1.disp();
+    t2.disp();
+  end
+endmodule
+    
+  
