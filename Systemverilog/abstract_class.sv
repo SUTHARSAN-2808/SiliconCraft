61b0350aa2474174b695b0165ce89f2e51e1fca5
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-05-05 11:45:24 +0530

    Update abstract_class.sv

--- Systemverilog/abstract_class.sv
+++ Systemverilog/abstract_class.sv
@@ -1,1 +1,24 @@
+//Abstract class
+virtual class baseclass;
+  bit[11:0]data;
+  function new();
+    data=5;
+  endfunction
+  function  void display();
+    $display(data);
+  endfunction
+endclass
 
+class child extends baseclass;
+  function display();
+    $display(data);
+  endfunction
+endclass
+
+    module ab;
+  initial begin
+    child bc;
+    bc=new();
+    bc.display();
+  end
+endmodule
