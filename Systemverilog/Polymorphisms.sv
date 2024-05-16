ddcc81cbc9c198b90cece5f39a14f2446060f204
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-05-16 11:13:35 +0530

    Update Polymorphisms.sv

--- Systemverilog/Polymorphisms.sv
+++ Systemverilog/Polymorphisms.sv
@@ -1,1 +1,31 @@
+//polymorphisms
 
+class  base;
+  virtual function display();
+    $display("inside base class");
+  endfunction
+endclass
+
+class ex1 extends base;
+  function display();
+    $display("inside ex1 class");
+  endfunction
+endclass
+
+class ex2 extends base;
+  function display();
+    $display("inside ex2 class");
+  endfunction
+endclass
+
+module new1;
+  initial begin
+    ex1 e1=new();
+    ex2 e2=new();
+    base b[2];
+    b[0]=e1;
+    b[1]=e2;
+    b[0].display();
+    b[1].display();
+  end
+endmodule
