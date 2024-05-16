65d69d28fc553263db3583183079229090cfed44
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-05-16 11:22:36 +0530

    Update Encapsulation_localvar.sv

--- Systemverilog/Encapsulation_localvar.sv
+++ Systemverilog/Encapsulation_localvar.sv
@@ -1,1 +1,19 @@
+class new1;
+  local int addr;
+  
+  function new(bit [4:0] raddr);
+   addr=raddr+9;
+  endfunction
+  
+  function disp();
+    $display(addr);
+  endfunction
+endclass
 
+module localvar;
+  initial begin
+    new1 n=new(6);
+   // n.addr=8;
+    n.disp();
+  end
+endmodule
