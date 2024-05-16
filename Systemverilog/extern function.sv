9ec093b93b10681a84ede208f826d287e6e573a1
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-05-16 11:28:06 +0530

    Update extern function.sv

--- Systemverilog/extern function.sv
+++ Systemverilog/extern function.sv
@@ -1,1 +1,18 @@
+//External task
+class ext;
+  int data;
+  extern task disp();
+ 
+endclass
 
+task ext::disp();
+  $display(data);
+endtask
+
+module sc1;
+  initial begin
+    ext t=new();
+    t.data=99;
+    t.disp();
+  end
+endmodule
