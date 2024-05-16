764ea64bcb2209acebb8111d796a6c1f9cc9c203
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-05-16 11:24:53 +0530

    Update Encapsulation protected.sv

--- Systemverilog/Encapsulation protected.sv
+++ Systemverilog/Encapsulation protected.sv
@@ -1,1 +1,28 @@
+class new1;
+ protected int addr;
+  function new(int raddr);
+    addr=raddr+10;
+  endfunction
+endclass
 
+class new2 extends new1;
+  function new(int raddr);
+    super.new(raddr);
+  endfunction
+  
+  function incraddr();
+    addr++;
+  endfunction
+  
+  function disp();
+    $display(addr);
+  endfunction
+endclass
+
+module protectedvar;
+  initial begin
+    new2 n2=new(8);
+    n2.incraddr();
+    n2.disp();
+  end
+endmodule
