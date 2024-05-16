08e69a583625c4f8af4d4ba7e24edcd880f98fea
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-05-16 11:16:43 +0530

    Update Dynamic casting.sv

--- Systemverilog/Dynamic casting.sv
+++ Systemverilog/Dynamic casting.sv
@@ -1,1 +1,29 @@
+//Dynamic casting
+class base;
+  int addr;
+  function disp();
+    $display(addr);
+  endfunction
+endclass
 
+class child extends base;
+  int data;
+  function disp();
+    super.disp();
+    $display(data);
+  endfunction
+endclass
+
+module d_casting;
+  child c=new();
+  child c1;
+  base b;
+  initial begin
+    c.addr=7;
+    c.data=9;
+    b=c;
+    $cast(c1,b);
+    c1.disp();
+  end
+endmodule
+    
