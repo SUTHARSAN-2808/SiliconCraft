8e83226bff7d2740db3b2f20957a4969b5ef3579
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-05-16 11:26:14 +0530

    Update scope resolution.sv

--- Systemverilog/scope resolution.sv
+++ Systemverilog/scope resolution.sv
@@ -1,1 +1,22 @@
+//Scope resolution operator
+class scope;
+ 
+   static int data;
+  
+  function disp(int a,int b);
+    $display(a,b);
+  endfunction
+endclass
 
+module ak;
+  
+  initial begin
+     
+  
+    scope s=new();
+    int data=89;
+       scope::data=8;
+    
+    s.disp(scope::data,data);
+  end
+endmodule
