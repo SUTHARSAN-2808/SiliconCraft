866856e758bbb1dd17458d1098180d4084432046
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-05-16 11:12:02 +0530

    Update Virtual task and function.sv

--- Systemverilog/Virtual task and function.sv
+++ Systemverilog/Virtual task and function.sv
@@ -1,1 +1,22 @@
+//with virtual tasks and functions
+  class base;
+  virtual function display;
+    $display("Outside extended class");
+  endfunction
+endclass
 
+class extended extends base;
+  function display;
+    $display("Inside extended class");
+  endfunction
+endclass
+
+module new1;
+  initial begin
+    base b;
+    extended e;
+    e=new();
+    b=e;
+    b.display();
+  end
+endmodule
