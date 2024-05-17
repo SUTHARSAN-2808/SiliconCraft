60423cfe67bb736a60a49976fd6d2e4a3b887fb6
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-05-17 09:53:37 +0530

    Update fork_join.sv

--- Systemverilog/fork_join.sv
+++ Systemverilog/fork_join.sv
@@ -1,1 +1,24 @@
+module fork_join;
 
+    
+    task a();
+      $display($time,"-----------");
+      $display($time,"Process 1 started");
+      #10 $display($time,"Process 1 finished");
+    endtask
+    
+    task b();
+      $display($time,"Process 2 started");
+      #15 $display($time,"Process 2 finished");
+    endtask
+    
+
+  
+  initial begin
+    fork
+      a();
+      b();
+    join
+  end
+  
+endmodule
