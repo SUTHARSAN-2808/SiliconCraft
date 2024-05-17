4d54f2b20c6667540d0383a2b0ca1c2b4a88ce6e
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-05-17 10:13:19 +0530

    Update Disable fork.sv

--- Systemverilog/Disable fork.sv
+++ Systemverilog/Disable fork.sv
@@ -1,1 +1,22 @@
-
+module disable_fork;
+  
+  task a();
+    $display("1 started");
+    #5 $display("1 finished");
+  endtask
+  
+   task b();
+     $display("2 started");
+     #8 $display("2 finished");
+  endtask
+  
+  initial begin
+    fork 
+      a();
+      b();
+    join_any
+    disable fork;
+    $display("completed");
+  end
+  
+endmodule
