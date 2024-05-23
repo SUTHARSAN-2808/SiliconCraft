692665934729bd96a580574887cb83e5f0210f1d
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-05-23 18:19:47 +0530

    Update Event_triggering.sv

--- Systemverilog/Event_triggering.sv
+++ Systemverilog/Event_triggering.sv
@@ -1,1 +1,24 @@
 
+module event_triggering;
+  event c;
+  
+  task a();
+    $display($time," Process 1 is starting");
+    ->c;
+    $display($time," Event is triggered");
+  endtask
+  
+  task b();
+    #4 $display($time, " Event is going to trigger");
+    @c;
+    $display($time,"Event is triggered");
+  endtask
+  
+  initial begin
+    fork
+      a();
+      b();
+    join
+  end
+endmodule
+  
