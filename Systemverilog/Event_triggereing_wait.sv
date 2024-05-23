e97e9cd138947e340371f09cd77ca4494018063c
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-05-23 18:20:26 +0530

    Update Event_triggereing_wait.sv

--- Systemverilog/Event_triggereing_wait.sv
+++ Systemverilog/Event_triggereing_wait.sv
@@ -1,1 +1,24 @@
-
+//event triggering using wait
+module event_triggering;
+  event c;
+  
+  task a();
+    $display($time," Process 1 is starting");
+    ->c;
+    $display($time," Event is triggered");
+  endtask
+  
+  task b();
+    #4 $display($time, " Event is going to trigger");
+    wait(c.triggered);
+    $display($time,"Event is triggered");
+  endtask
+  
+  initial begin
+    fork
+      a();
+      b();
+    join
+  end
+endmodule
+  
