a355b64790ea8b55d7ed1450ddc59c58c31be50d
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-06-24 11:40:46 +0530

    Update Environment.sv

--- Verification/Counter/Environment.sv
+++ Verification/Counter/Environment.sv
@@ -1,1 +1,39 @@
+`include "Generator.sv"
+`include "Driver.sv"
+`include "Scoreboard.sv"
+`include "Monitor.sv"
+//`include "Transaction.sv"
 
+class environment;
+  
+  generator g;
+  driver d;
+  monitor m;
+  scoreboard sco;
+  transaction t;
+  mailbox mail;
+  virtual inter vi;
+  
+  function new(virtual inter vi,mailbox mail);
+    this.vi=vi;
+    this.mail=mail;
+  endfunction
+  
+  task memory;
+    t=new();
+    m=new(vi,mail);
+    sco=new(mail);
+    g=new(mail);
+    d=new(vi,mail);
+  endtask
+  
+  task run;
+    fork
+    g.gen;
+    m.mon;
+    sco.score;
+    d.drive;
+    join
+  endtask
+  
+endclass
