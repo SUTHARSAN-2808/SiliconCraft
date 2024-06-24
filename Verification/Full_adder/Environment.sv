8964f5781b18658c5452098e5556d730d43319a0
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-06-24 10:47:57 +0530

    Update Environment.sv

--- Verification/Full_adder/Environment.sv
+++ Verification/Full_adder/Environment.sv
@@ -1,1 +1,38 @@
+`include "Transaction.sv"
+`include "Driver.sv"
+`include "Generator.sv"
+`include "Monitor.sv"
+`include "Scoreboard.sv"
 
+class environment;
+  driver d;
+  monitor mo;
+  scoreboard s;
+  generator g;
+  virtual inter vi;
+  mailbox m;
+  transaction t;
+  
+  function new(virtual inter vi,mailbox m);
+    this.vi=vi;
+    this.m=m;
+  endfunction
+  
+  task memory;
+  t=new();
+    mo=new(vi,m);
+    s=new(m);
+    g=new(m);
+    d=new(vi,m);
+  endtask
+  
+  task run;
+    fork
+    s.sco;
+    mo.mon;
+    g.gene;
+    d.dri;
+    join
+  endtask
+  
+endclass
