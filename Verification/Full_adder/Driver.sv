31df3433ee056baab05f33ce0a9a9d688b7f0552
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-06-24 10:46:31 +0530

    Update Driver.sv

--- Verification/Full_adder/Driver.sv
+++ Verification/Full_adder/Driver.sv
@@ -1,1 +1,27 @@
 
+class driver;
+  virtual inter vi;
+  transaction t;
+  mailbox m;
+  
+  function new(virtual inter vi,mailbox m);
+    this.vi=vi;
+    this.m=m;
+  endfunction
+  
+  task dri;
+    repeat(5) begin
+     // #10
+    t=new();
+    m.get(t);
+    vi.a=t.a;
+    vi.b=t.b;
+    vi.cin=t.cin;
+    #10
+    t.sum=vi.sum;
+    t.cout=vi.cout;
+   // $display("a=%d b=%d cin=%d sum=%d cout=%d",vi.a,vi.b,vi.cin,t.sum,t.cout);
+     #20;
+    end
+      endtask
+endclass
