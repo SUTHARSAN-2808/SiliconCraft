81b2d84b76786893aeb26bbf0dd9390532bfacbf
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-06-24 11:40:04 +0530

    Update Driver.sv

--- Verification/Counter/Driver.sv
+++ Verification/Counter/Driver.sv
@@ -1,1 +1,31 @@
-
+class driver;
+  
+  transaction t;
+  mailbox mail;
+  virtual inter vi;
+  
+  function new(virtual inter vi, mailbox mail);
+    this.vi=vi;
+    this.mail=mail;
+  endfunction
+  
+  task drive;
+    t=new();
+    mail.get(t);
+    
+    vi.reset=1'b1;
+    #3 
+    vi.reset=1'b0;
+    vi.d=1'd1;
+    #100
+    vi.d=1'b0;
+  
+    t.out=vi.out;
+    #195
+    $display("---------------------------------------------");
+    $display($time,"Entire process completed");
+      $display("--------------------------------------------");
+   
+  
+  endtask
+endclass
