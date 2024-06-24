21e1dd100bdd3e6ab28a1cc579e83cb92aba9dc4
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-06-24 11:37:11 +0530

    Update Design.sv

--- Verification/Counter/Design.sv
+++ Verification/Counter/Design.sv
@@ -1,1 +1,15 @@
-
+//UP counter
+module counter2(input clk,
+               input reset,
+                input d,
+                output  reg [3:0] out );
+  
+  always @ (posedge clk)begin
+    if(reset==1'b1 || out==4'b0101)
+      out<=4'b0;
+    else if(d==1'b1)
+      out<=out+1;
+    else
+      out<=out-1;
+  end
+endmodule
