a28e4ceaa4e20d65b004745e670dc00cc0b006a4
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-06-24 10:49:58 +0530

    Update Scoreboard.sv

--- Verification/Full_adder/Scoreboard.sv
+++ Verification/Full_adder/Scoreboard.sv
@@ -1,1 +1,25 @@
 
+class scoreboard;
+  transaction t;
+  mailbox mail;
+  
+  function new(mailbox mail);
+    this.mail=mail;
+  endfunction
+  
+  task sco;
+  //  #10
+    forever  #10 begin
+    mail.get(t);
+      if(t.sum==1'b1 && t.cout==1'b0)begin
+      $display("a=%d b=%d cin=%d sum=%d cout=%d",t.a,t.b,t.cin,t.sum,t.cout);
+      $display("Sum is high");
+      end
+    else begin
+      $display("a=%d b=%d cin=%d sum=%d cout=%d",t.a,t.b,t.cin,t.sum,t.cout);
+      $display("Carry is high");
+    end
+    end
+  endtask
+  
+endclass
