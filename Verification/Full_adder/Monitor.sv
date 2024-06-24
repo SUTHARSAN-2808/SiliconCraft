eed49be42d498d7355b14a932ebdd7b2824e2e55
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-06-24 10:49:13 +0530

    Update Monitor.sv

--- Verification/Full_adder/Monitor.sv
+++ Verification/Full_adder/Monitor.sv
@@ -1,1 +1,25 @@
 
+class monitor;
+  transaction t;
+  virtual inter vi;
+  mailbox m;
+  
+  function new(virtual inter vi,mailbox m);
+    this.vi=vi;
+    this.m=m;
+  endfunction
+  
+  task mon;
+    #10
+    forever  #10 begin
+    t=new();
+    t.a=vi.a;
+    t.b=vi.b;
+    t.cin=vi.cin;
+    t.sum=vi.sum;
+    t.cout=vi.cout;
+    m.put(t);
+    end
+    
+  endtask
+endclass
