4551e31f03b0797e04d83b0ef958d01a792fb370
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-06-24 11:39:29 +0530

    Update Generator.sv

--- Verification/Counter/Generator.sv
+++ Verification/Counter/Generator.sv
@@ -1,1 +1,17 @@
-
+`include "Transaction.sv" 
+class generator;
+  
+  transaction t;
+  mailbox mail;
+  
+  function new(mailbox mail);
+    this.mail=mail;
+  endfunction
+  
+  task gen;
+    t=new();
+    mail.put(t);
+    $display("Generator block initiated");
+  endtask
+  
+endclass
