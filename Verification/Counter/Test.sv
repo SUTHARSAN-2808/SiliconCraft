e390d096593572b3644992265c20e6d36699e958
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-06-24 11:41:32 +0530

    Update Test.sv

--- Verification/Counter/Test.sv
+++ Verification/Counter/Test.sv
@@ -1,1 +1,26 @@
+`include "Environment.sv"
 
+class test;
+  environment env;
+  transaction t;
+  mailbox mail;
+  virtual inter vi;
+  
+  function new(virtual inter vi,mailbox mail);
+    this.vi=vi;
+    this.mail=mail;
+  endfunction
+  
+  task memory;
+    t=new();
+    env=new(vi,mail);
+  endtask
+  
+  task run;
+    fork
+    env.memory;
+    env.run;
+    join
+  endtask
+  
+endclass
