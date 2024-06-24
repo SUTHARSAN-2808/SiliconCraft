9b19a2db303c148309e2c8a6a88acd1c78b67996
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-06-24 11:42:09 +0530

    Update Monitor.sv

--- Verification/Counter/Monitor.sv
+++ Verification/Counter/Monitor.sv
@@ -1,1 +1,23 @@
-
+class monitor;
+  mailbox mail;
+  transaction t;
+  virtual inter vi;
+  
+  function new(virtual inter vi,mailbox mail);
+    this.vi=vi;
+    this.mail=mail;
+  endfunction
+  
+  task mon;
+  forever #6 begin
+    t=new();
+    t.clk=vi.clk;
+    t.reset=vi.reset;
+    t.d=vi.d;
+    t.out=vi.out;
+      mail.put(t);
+  end
+  endtask
+  
+  
+endclass
