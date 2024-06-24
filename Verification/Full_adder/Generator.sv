bff5643e8b52ea9dbdce58129a7cb67d0e68912e
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-06-24 10:45:56 +0530

    Update Generator.sv

--- Verification/Full_adder/Generator.sv
+++ Verification/Full_adder/Generator.sv
@@ -1,1 +1,24 @@
 
+class generator;
+  transaction t;
+  mailbox m;
+  
+  function new(mailbox m);
+    this.m=m;
+  endfunction
+  
+  task gene;
+    repeat(5)begin
+     // #10
+    t=new();
+    t.randomize();
+    m.put(t);
+      #30;
+   
+   // $display("packe is sent to driver");
+   end
+  endtask
+    
+endclass
+
+
