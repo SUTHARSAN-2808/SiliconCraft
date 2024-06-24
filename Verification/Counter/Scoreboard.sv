02289084de580c78857725c23855e5d02537bc7e
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-06-24 11:42:44 +0530

    Update Scoreboard.sv

--- Verification/Counter/Scoreboard.sv
+++ Verification/Counter/Scoreboard.sv
@@ -1,1 +1,18 @@
-
+class scoreboard;
+  mailbox mail;
+  transaction t;
+  
+  function new(mailbox mail);
+    this.mail=mail;
+  endfunction
+  
+  task score;
+   forever #6 begin
+    mail.get(t);
+     if(t.out>4'b0011)
+        $display("++++++++++++TARGET PASSED +++++++++++++++++");
+      else
+       $display("++++++++++++TARGET FAILED +++++++++++++++++");
+   end
+  endtask
+endclass
