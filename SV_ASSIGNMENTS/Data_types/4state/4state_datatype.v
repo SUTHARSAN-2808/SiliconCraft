ba690d4c44233ef58dfeceb546c3d2fa848dfb75
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-06-25 10:33:11 +0530

    Update 4state_datatype.v

--- SV_ASSIGNMENTS/Data_types/4state/4state_datatype.v
+++ SV_ASSIGNMENTS/Data_types/4state/4state_datatype.v
@@ -1,1 +1,39 @@
+module four_state_datatypes;
+  
+  reg rdata;  // >=1 bit unsigned
+  logic ldata;   //  >=1 bit unsigned
+  integer idata;    //32 bit signed
+  time tdata;  //64 bit unsigned
+  real redata;   //64 bit unsigned
+  wire wdata;  //>=1 bit unsigned
+  
+  initial begin
+    
+    
+    $display("Before initialization");
+    $display("rdata=%b",rdata);
+    $display("ldata=%b",ldata);
+    $display("idata=%b",idata);
+    $display("tdata=%b",tdata);
+    $display("redata=%b",redata);
+    $display("wdata=%b",wdata);
+    
+    
+    $display("After initialization");
+    rdata=3'bx11;
+    ldata=4'b0z1;
+    idata=32'h10xz_xz11;
+    tdata=64'hzzxx_xx00_1z1z_x0xz;
+    redata=64'hzzzz_xxxx_1111_0000;
 
+    $display("rdata=%b",rdata);
+    $display("ldata=%b",ldata);
+    $display("idata=%b",idata);
+    $display("tdata=%b",tdata);
+    $display("redata=%b",redata);
+
+    
+  end
+  
+  
+endmodule
