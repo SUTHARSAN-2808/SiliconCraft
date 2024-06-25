7279901d62b22ff6c9302c215994f4199a5876bd
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-06-25 14:02:56 +0530

    Update unpacked_structures.v

--- SV_ASSIGNMENTS/Structures/Unpacked_Structures/unpacked_structures.v
+++ SV_ASSIGNMENTS/Structures/Unpacked_Structures/unpacked_structures.v
@@ -1,1 +1,12 @@
+//unpacked structure
+struct { string name;
+        reg [7:0] id;
+        logic [7:0] address;
+       } employee_details;
 
+module emp;
+  initial begin
+  employee_details ='{"Great",76989,2430};
+  $display(employee_details);
+  end
+endmodule
