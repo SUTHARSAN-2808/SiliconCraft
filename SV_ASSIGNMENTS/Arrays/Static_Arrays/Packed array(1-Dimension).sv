59a11698ac3db9ae7f264172b1aa1591e06f1868
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-06-25 18:41:11 +0530

    Update Packed array(1-Dimension).sv

--- SV_ASSIGNMENTS/Arrays/Static_Arrays/Packed array(1-Dimension).sv
+++ SV_ASSIGNMENTS/Arrays/Static_Arrays/Packed array(1-Dimension).sv
@@ -1,1 +1,14 @@
-
+//fixed array(Packed) single dimension
+module fixed_Arrays;
+  
+  bit [3:0] a;
+  
+  initial begin
+    a=4'b1010;
+    $display(a);
+    foreach(a[i])begin
+      $display("a[%d]=%b",i,a[i]);
+    end
+  end
+  
+endmodule
