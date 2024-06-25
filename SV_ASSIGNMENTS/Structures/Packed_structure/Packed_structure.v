61d656d9866bb9c64ea89394824b8dc051a53037
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-06-25 14:19:16 +0530

    Update Packed_structure.v

--- SV_ASSIGNMENTS/Structures/Packed_structure/Packed_structure.v
+++ SV_ASSIGNMENTS/Structures/Packed_structure/Packed_structure.v
@@ -1,1 +1,12 @@
+//packed structure
+struct packed {byte proof;
+        bit [7:0] id;
+        logic [7:0] address;
+       } employee_details;
 
+module emp;
+  initial begin
+    employee_details ='{00000000,00010000,0000001};
+  $display(employee_details);
+  end
+endmodule
