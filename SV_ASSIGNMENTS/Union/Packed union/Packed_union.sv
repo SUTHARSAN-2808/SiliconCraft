2990792db7c77cd069a50b6c7ad098f08dc6fd94
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-06-25 18:19:30 +0530

    Update Packed_union.sv

--- SV_ASSIGNMENTS/Union/Packed union/Packed_union.sv
+++ SV_ASSIGNMENTS/Union/Packed union/Packed_union.sv
@@ -1,1 +1,11 @@
-
+union packed {bit [3:0] a;
+              bit [3:0] b;
+             }datum;
+module union_packed;
+      
+  initial begin
+    datum.a=4'b1010;
+ //   datum.b=4'b0001;
+    $display(datum);
+  end
+endmodule
