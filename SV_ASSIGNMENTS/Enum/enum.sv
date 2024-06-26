6c93388037bf89c9dc81a6602ef4b32a8f8da77f
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-06-26 19:22:06 +0530

    Update enum.sv

--- SV_ASSIGNMENTS/Enum/enum.sv
+++ SV_ASSIGNMENTS/Enum/enum.sv
@@ -1,1 +1,15 @@
-
+module enumm;
+  
+  enum {good,bad,ugly}nature;
+  
+  initial begin
+    
+    $display(nature.first);
+    $display(nature.next);
+    $display($size(nature));
+    for(byte i=0;i<$size(nature);i=i+1)begin
+      $display("Nature names is %s and its value=%0d",nature.name,nature);
+      nature=nature.next;
+    end
+  end
+endmodule
