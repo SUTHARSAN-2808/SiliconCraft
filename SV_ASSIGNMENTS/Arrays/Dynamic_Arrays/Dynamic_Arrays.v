ffcaabc6ba2172ce789a4d55155e8f43d994a002
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-06-26 14:13:33 +0530

    Update Dynamic_Arrays.v

--- SV_ASSIGNMENTS/Arrays/Dynamic_Arrays/Dynamic_Arrays.v
+++ SV_ASSIGNMENTS/Arrays/Dynamic_Arrays/Dynamic_Arrays.v
@@ -1,1 +1,15 @@
-
+//Dynamic array
+module dynamic_array;
+  int bd[];
+  int b[6]='{9,4,1,6,0,3};
+  
+  initial begin
+   bd=new[6];
+    foreach(bd[i])begin
+      $display("bd[%d]=%d",i,i*2);
+    end
+    b.sort();
+    $display(b);
+  end
+  
+endmodule
