1010f10a6d52ee8861ffadebc09139b35bb717e8
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-06-26 09:09:19 +0530

    Update Environment.sv

--- Verification/FIFO/Environment.sv
+++ Verification/FIFO/Environment.sv
@@ -1,1 +1,29 @@
+`include "generator.sv"
+`include "driver.sv"
 
+class environment;
+  //transaction t;
+  mailbox mail;
+  generator g;
+  driver d;
+  virtual inter vi;
+  
+  function new(mailbox mail,virtual inter vi);
+   this.mail=mail; 
+    this.vi=vi;
+  endfunction
+  
+  task memory;
+  // t=new();
+    g=new(mail);
+    d=new(mail,vi);
+  endtask
+  
+  task run;
+    fork
+      g.gen;
+      d.run;
+    join
+  endtask
+  
+endclass
