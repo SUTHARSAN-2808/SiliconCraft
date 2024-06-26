0feb654ac29f9ba5eac4e467635fcf8e609fc9b0
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-06-26 09:09:50 +0530

    Update Test.sv

--- Verification/FIFO/Test.sv
+++ Verification/FIFO/Test.sv
@@ -1,1 +1,25 @@
+`include "environment.sv"
 
+class test;
+  environment env;
+ // transaction t;
+  mailbox mail;
+  virtual inter vi;
+  
+  function new(virtual inter vi,mailbox mail);
+   this.vi=vi;
+    this.mail=mail;
+  endfunction
+  
+  task memory;
+   // t=new();
+    env=new(mail,vi);
+  endtask
+  
+  task run;
+    fork
+     env.memory;
+      env.run;
+    join
+  endtask
+endclass
