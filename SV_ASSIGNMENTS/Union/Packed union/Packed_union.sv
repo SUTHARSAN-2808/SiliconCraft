45f1edf28378b6c31a918145e7534a3729bfefbf
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-06-26 12:55:32 +0530

    Update Packed_union.sv

--- SV_ASSIGNMENTS/Union/Packed union/Packed_union.sv
+++ SV_ASSIGNMENTS/Union/Packed union/Packed_union.sv
@@ -1,11 +1,19 @@
-union packed {bit [3:0] a;
-              bit [3:0] b;
-             }datum;
-module union_packed;
-      
+
+
+union packed {byte a;
+       byte b;
+      }datas;
+
+module da;
   initial begin
-    datum.a=4'b1010;
- //   datum.b=4'b0001;
-    $display(datum);
+   datas.a=8'hca;
+    datas.b=8'hea;
+    $display("a=%h",datas.a);
+    $display("b=%h",datas.b);
+    
+      datas.b=8'hef;
+     $display("a=%h",datas.a);
+    $display("b=%h",datas.b);
+    
   end
 endmodule
