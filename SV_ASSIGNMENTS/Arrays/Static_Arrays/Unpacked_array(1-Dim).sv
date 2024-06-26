3fd6e82674dc9e4c58ba109389bc5e6f9f9cfb92
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-06-26 10:26:49 +0530

    Update Unpacked_array(1-Dim).sv

--- SV_ASSIGNMENTS/Arrays/Static_Arrays/Unpacked_array(1-Dim).sv
+++ SV_ASSIGNMENTS/Arrays/Static_Arrays/Unpacked_array(1-Dim).sv
@@ -1,1 +1,14 @@
-
+//unpacked 1dim array
+module unpacked_1dim_array;
+  
+  int  a[4:0] ;
+  
+  initial begin
+ 
+    a={46,23,123,600,1};
+    $display(a);
+    foreach(a[i])
+      $display("a[%d]=%b",i,a[i]);
+  end
+  
+endmodule
