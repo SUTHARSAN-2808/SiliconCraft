e21868e6648da1d8519c7b7e517fe18ab496c5a4
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-06-26 15:24:02 +0530

    Update Associative_array.sv

--- SV_ASSIGNMENTS/Arrays/Associative_Arrays/Associative_array.sv
+++ SV_ASSIGNMENTS/Arrays/Associative_Arrays/Associative_array.sv
@@ -1,1 +1,14 @@
-A
+//Associative_array
+module associative_array;
+  
+  int a[*];
+  string b[string];
+  
+  initial begin
+    a='{2:20,3:5,90:6,7:1};
+    b='{"vegetables": "Guava" ,"Season": "Rainy" };
+    $display("int type a=%p",a);
+    $display("String type b=%p",b);
+  end
+  
+endmodule
