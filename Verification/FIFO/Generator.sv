096e42e95aa8ae72977c10b1ca31f49c2b1d54bb
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-06-26 09:08:20 +0530

    Update Generator.sv

--- Verification/FIFO/Generator.sv
+++ Verification/FIFO/Generator.sv
@@ -1,1 +1,21 @@
 
+`include "transaction.sv"
+class generator;
+  transaction t;
+  mailbox mail;
+  
+  function new(mailbox mail);
+    this.mail=mail;
+  endfunction
+  
+  task gen;
+   
+    repeat(10)begin
+      t=new();
+      t.randomize();
+     // t.datain=4'b1100;
+    mail.put(t);
+    end
+  endtask
+endclass
+
