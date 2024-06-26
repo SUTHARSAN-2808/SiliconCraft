53fd4bb64e1093e6327ced9427c439cbb04826bc
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-06-26 19:48:59 +0530

    Update typedef_enum.sv

--- SV_ASSIGNMENTS/Typedef_enum/typedef_enum.sv
+++ SV_ASSIGNMENTS/Typedef_enum/typedef_enum.sv
@@ -1,1 +1,31 @@
 
+
+module typedef_enum;
+  
+  
+  typedef enum {MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY }week_e;
+  week_e day; 
+    
+  initial begin
+
+    $display("\n//days = {\n MONDAY,\n TUESDAY,\n WEDNESDAY,\n THURSDAY,\n FRIDAY,\n SATURDAY,\n SUNDAY\n }");
+    day = day.first();
+    $display("\nfirst day name = %0s  and its value is = %0d",day,day);
+
+    day = day.last();  
+    $display("\nlast day name = %0s  and its value is = %0d",day,day);
+
+    day = WEDNESDAY; 
+    day = day.next();
+    $display("\nnext day name after wednesday  = %0s  and its value is = %0d",day,day);
+
+    day = THURSDAY;
+    day = day.prev();  
+    $display("\nprevious day name befor thursday  = %0s  and its value is = %0d",day,day);
+
+    $display("\ncurrent day name = %0s  and its value is = %0d",day.name(),day);
+
+    $display("\ntotal number of days in week type is = %0d\n",day.num());
+ 
+  end
+endmodule : typedef_enum
