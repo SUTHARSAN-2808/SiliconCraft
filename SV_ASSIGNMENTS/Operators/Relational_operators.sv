f9e5dd9aa5b25d3472ee024164cab626f27cd57c
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-06-26 21:14:31 +0530

    Update Relational_operators.sv

--- SV_ASSIGNMENTS/Operators/Relational_operators.sv
+++ SV_ASSIGNMENTS/Operators/Relational_operators.sv
@@ -1,1 +1,16 @@
-
+module relational_operator;
+  reg [3:0] a,b;
+  int y;
+  initial begin
+    a=4'b1101;
+    b=4'b1001;
+   
+    $display(a>b);
+   
+    $display(a<b);
+   
+    $display(a>=b);
+   
+    $display(a<=b);
+  end
+endmodule
