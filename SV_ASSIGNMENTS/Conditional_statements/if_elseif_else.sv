5364a80728050915b516467755c36a9c10c918a6
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-06-27 13:56:21 +0530

    Update if_elseif_else.sv

--- SV_ASSIGNMENTS/Conditional_statements/if_elseif_else.sv
+++ SV_ASSIGNMENTS/Conditional_statements/if_elseif_else.sv
@@ -1,1 +1,27 @@
-
+module if_elseif_else;
+int a , b,c ;
+initial begin 
+ a = -12;
+ b = 12;
+ c = 10;
+ 
+ if (a>0) //false conditon 
+ begin 
+ $display ("Entered into if block ");
+ $display ("a is a negative number");
+ end 
+ else if (a==0) begin  // false condition  
+   $display ("Entered into first elseif block ");
+   $display ("a is a negative number ");
+ end 
+   else if (b<c)begin // false condition
+     $display ("Entered into second elseif block ");
+     $display ("b is less than c ");
+   end 
+   else begin 
+     $display ("None of the conditon is true in if-elseif blocks ");
+  
+   end 
+   $display ("Out of the conditional block ");
+ end 
+ endmodule 
