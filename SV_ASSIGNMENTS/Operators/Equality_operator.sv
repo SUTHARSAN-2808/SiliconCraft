b72aeb36e8af767d0aca85a3779504e4d1921760
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-06-27 10:03:41 +0530

    Update Equality_operator.sv

--- SV_ASSIGNMENTS/Operators/Equality_operator.sv
+++ SV_ASSIGNMENTS/Operators/Equality_operator.sv
@@ -1,1 +1,12 @@
-
+//Equality operators
+module eq_operators;
+  integer a,b;
+  initial begin
+    a=4'b1101;
+    b=4'b11x1;
+    $display("Equaltiy a==b ",a==b);
+    $display("Non Equality a!=b ",a!=b);
+    $display("Case Equaltiy ",a===b);
+    $display("Case Non Equaltiy ",a!==b);
+  end
+endmodule
