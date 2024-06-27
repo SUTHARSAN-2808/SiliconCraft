547b6e6352a3b90a3b687e7da3770bdbb32a428c
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-06-27 16:26:36 +0530

    Update Priority case.sv

--- SV_ASSIGNMENTS/Case/Priority case.sv
+++ SV_ASSIGNMENTS/Case/Priority case.sv
@@ -1,1 +1,14 @@
-
+//priority case
+module pcase;
+  bit [1:0]a;
+  initial begin
+    priority case(a)
+      2'b00:$display("5");
+      2'b01:$display("7");
+      2'b00:$display("6");
+      2'b11:$display("9");
+      
+      
+    endcase
+  end
+endmodule
