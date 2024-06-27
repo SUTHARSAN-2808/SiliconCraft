01432b51872f2831172f7f9a8f63f5e23deb12e9
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-06-27 12:51:18 +0530

    Update if_else.sv

--- SV_ASSIGNMENTS/Conditional_statements/if_else.sv
+++ SV_ASSIGNMENTS/Conditional_statements/if_else.sv
@@ -1,1 +1,15 @@
-
+module if_else;
+byte a;
+initial begin
+  a = -1;
+  
+  if(a>0)begin
+    $display ("Successfully enters into if block");
+    $display ("a is a postive number");
+ 
+  end 
+  else 
+    $display ("Number is negative");
+  $display ("Out of if else block");
+end 
+endmodule 
