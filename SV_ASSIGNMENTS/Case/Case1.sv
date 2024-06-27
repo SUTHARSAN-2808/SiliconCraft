f29b7e1b8f9c0e081ef7dee9003e3b6d004ce907
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-06-27 15:48:05 +0530

    Update Case1.sv

--- SV_ASSIGNMENTS/Case/Case1.sv
+++ SV_ASSIGNMENTS/Case/Case1.sv
@@ -1,1 +1,16 @@
 
+module case1;
+  
+  reg [1:0]x;
+  initial begin
+    x=2'd3;
+  case(x)
+    
+    2'b00:$display("0");
+    2'b01:$display("1");
+    2'b10:$display("2");
+    2'b11:$display("3");
+    
+  endcase
+  end
+endmodule
