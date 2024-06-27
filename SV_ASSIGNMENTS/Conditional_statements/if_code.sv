407f19baf51e0281e76504304684450f9b494361
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-06-27 12:46:50 +0530

    Update if_code.sv

--- SV_ASSIGNMENTS/Conditional_statements/if_code.sv
+++ SV_ASSIGNMENTS/Conditional_statements/if_code.sv
@@ -1,1 +1,15 @@
-
+module if_code;
+bit [3:0]a;
+initial begin 
+    a=6;
+    
+  $display(a);
+  if (a==6)begin 
+      $display ("if expression is true , Successfully entered into the if block");
+    $display ( "a is equal to 6 " );
+     
+    end 
+     $display("out of if block");
+  end 
+  endmodule 
+  
