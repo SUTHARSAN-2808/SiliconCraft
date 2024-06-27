400bb650a94ee0fb11173b3f3aee3a237d8cf960
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-06-27 14:31:57 +0530

    Update priority_if(More than one cond).sv

--- SV_ASSIGNMENTS/Conditional_statements/priority_if(More than one cond).sv
+++ SV_ASSIGNMENTS/Conditional_statements/priority_if(More than one cond).sv
@@ -1,1 +1,24 @@
 
+module priorityif_2;
+int a,b,c;
+initial begin 
+  a = 10;
+  b =20;
+  c = 30;
+
+priority if (a>b)begin 
+  $display ("Inside the priority if block ");
+  $display (" a <b");
+end 
+else if ( b <c )begin 
+  $display ("Inside the first else if block");
+  $display ("b<c");
+end 
+else if (a <c )begin 
+  $display ("Inside the second else if block ");
+  $display ( "a < c ");
+end 
+$display ("Out from the conditional block ");
+
+end 
+endmodule 
