1584d45e1e49590fc18ab75de99fde946e69551c
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-06-27 13:03:31 +0530

    Update if_elseif.sv

--- SV_ASSIGNMENTS/Conditional_statements/if_elseif.sv
+++ SV_ASSIGNMENTS/Conditional_statements/if_elseif.sv
@@ -1,1 +1,27 @@
+module if_elseif;
+int a,b,c;
+initial begin 
+ a = 10;
+ b = 12;
+ c = 13;
+ 
+ if (a>b)begin 
+   $display ("Successfully enters into if block ");
+   $display ("Value of a < b");
+   
+ end 
+ else if (b<c)begin 
+   $display ("Successfully enters into else if block ");
+   $display ("value of b<c");
+  
+   end
+   else if (c<0) begin 
+     $display ("Successfully enters into second elseif block ");
+     $display ("c is a negative number ");
+   
+   end 
+   $display ("Out from ladder block");
 
+ end 
+ endmodule 
+ 
