76b50e1bb6e37aa1fabbaf53145073b1a281d5df
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-06-28 07:16:31 +0530

    Update Function calling values from expression.sv

--- SV_ASSIGNMENTS/Functions/Function calling values from expression.sv
+++ SV_ASSIGNMENTS/Functions/Function calling values from expression.sv
@@ -1,1 +1,14 @@
-
+//Function calling with values from expression
+module func_argu;
+  bit [4:0] a,b,c,result;
+  initial begin
+    
+    $display("Answer is=%0d",sum(1,13));
+  end
+  
+  function int sum(int a,b);
+     c=a+b;
+    return c;
+    
+  endfunction
+endmodule
