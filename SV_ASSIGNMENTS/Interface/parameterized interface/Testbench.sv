9ea813d21bd06b4723c656fcc9c6287d32f47f7c
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-06-28 14:51:49 +0530

    Update Testbench.sv

--- SV_ASSIGNMENTS/Interface/parameterized interface/Testbench.sv
+++ SV_ASSIGNMENTS/Interface/parameterized interface/Testbench.sv
@@ -1,1 +1,16 @@
-
+`include "interface.sv"
+module upcounter_testbench(count_if inf);
+     initial begin
+     $display("\n // Parameterised interface example");
+    
+     $monitor("\ncount=%0d",inf.counter);
+     inf.clk=0;
+     forever #5 inf.clk=~inf.clk;
+     end
+     initial begin
+     inf.reset=1;
+     #20;
+     inf.reset=0;
+     #70 $finish;
+     end
+  endmodule
