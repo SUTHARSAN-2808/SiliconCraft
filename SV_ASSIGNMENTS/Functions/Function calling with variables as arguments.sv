6bf7150b75a425840f2fb0f7b6c60a9b4cb8aac6
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-06-28 07:10:15 +0530

    Update Function calling with variables as arguments.sv

--- SV_ASSIGNMENTS/Functions/Function calling with variables as arguments.sv
+++ SV_ASSIGNMENTS/Functions/Function calling with variables as arguments.sv
@@ -1,1 +1,15 @@
-
+//Function calling with variables as arguments
+module func_variables;
+  int c,d,ans,result;
+  function int mul(int c,d);
+    ans=c*d;
+    return ans;
+  endfunction
+  
+  initial begin
+    c=11;
+    d=7;
+    result=mul(c,d);
+    $display("Answer is %d",result);
+  end
+endmodule
