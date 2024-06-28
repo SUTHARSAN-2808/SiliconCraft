ed6061479f29f107e2e5c6cf170fd8f0583ad873
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-06-28 07:26:28 +0530

    Update Function with variables with positional arguments.sv

--- SV_ASSIGNMENTS/Functions/Function with variables with positional arguments.sv
+++ SV_ASSIGNMENTS/Functions/Function with variables with positional arguments.sv
@@ -1,1 +1,15 @@
-
+//Calling function with positional arguments
+module func_posiargu;
+  int a,b,c,result;
+  initial begin
+    result=sum(.b(3),.a(7));
+    
+    $display(result);
+  end
+  
+  function int sum(int a,b);
+    $display(a,b);
+    c=a+b;
+    return c;
+  endfunction
+endmodule
