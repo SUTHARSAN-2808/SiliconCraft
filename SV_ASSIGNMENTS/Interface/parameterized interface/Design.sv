4d254bb0c70e1723d4674467f2155b83ee5dc07e
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-06-28 14:49:44 +0530

    Update Design.sv

--- SV_ASSIGNMENTS/Interface/parameterized interface/Design.sv
+++ SV_ASSIGNMENTS/Interface/parameterized interface/Design.sv
@@ -1,1 +1,15 @@
-
+  module up_counter(clk,reset,counter);
+     input clk, reset;
+     output [2:0] counter;
+     reg [2:0] counter_up;
+    
+     always @(posedge clk or posedge reset)
+     begin
+    
+     if(reset)
+       counter_up <= 3'd0;
+       else
+       counter_up <= counter_up + 3'd1;
+     end 
+     assign counter = counter_up;
+     endmodule:
