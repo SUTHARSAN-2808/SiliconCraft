b1f6a016b5a55d8a73b8eac6a750efe6eea70958
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-06-28 10:43:01 +0530

    Update Void function.sv

--- SV_ASSIGNMENTS/Functions/Void function.sv
+++ SV_ASSIGNMENTS/Functions/Void function.sv
@@ -1,1 +1,12 @@
+module func_return_void;
+  
+  initial
+  begin
+    display("Hi");
+    display("\t passing string to function for displaying");
+  end
 
+  function void display(string str);
+    $display("%s",str);
+  endfunction 
+endmodule 
