f7b2ed7d4f4d4898ad00405461dd6b22b9b9e2e4
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-06-28 11:18:47 +0530

    Update Calling_task_in_function.sv

--- SV_ASSIGNMENTS/Functions/Calling_task_in_function.sv
+++ SV_ASSIGNMENTS/Functions/Calling_task_in_function.sv
@@ -1,1 +1,18 @@
-
+module calling_task_in_function;
+  
+  initial begin
+    $display("In initial begin");
+   #1 func;
+  end
+  
+  function func();
+    fork
+    $display("In func");
+     taskk;
+    join_none
+  endfunction
+  
+  task taskk;
+   #2 $display("In task");
+  endtask
+endmodule
