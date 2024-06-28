630ed515d8c6d5252a14f024f7cdcc7f6d1689b1
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-06-28 14:53:01 +0530

    Update Top.sv

--- SV_ASSIGNMENTS/Interface/parameterized interface/Top.sv
+++ SV_ASSIGNMENTS/Interface/parameterized interface/Top.sv
@@ -1,1 +1,13 @@
-
+`include "top.sv"
+   module top();
+   
+   
+     count_if inf();
+  
+   
+    up_counter u1(.clk(inf.clk), .reset(inf.reset), .counter(inf.counter));
+ 
+   
+    upcounter_testbench u2(inf);
+ 
+    endmodule:top
