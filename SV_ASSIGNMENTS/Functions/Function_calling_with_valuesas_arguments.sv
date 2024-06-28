07a70667ca3693a7b6e70664fa6eda910189531a
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-06-28 07:12:21 +0530

    Update Function_calling_with_valuesas_arguments.sv

--- SV_ASSIGNMENTS/Functions/Function_calling_with_valuesas_arguments.sv
+++ SV_ASSIGNMENTS/Functions/Function_calling_with_valuesas_arguments.sv
@@ -1,1 +1,14 @@
-
+//Function calling values as arguments
+module func_argu;
+  bit [4:0] a,b,c,result;
+  initial begin
+    result =sum(11,13);
+    $display("Answer is=%0d",result);
+  end
+  
+  function int sum(int a,b);
+     c=a+b;
+    return c;
+    
+  endfunction
+endmodule
