f606535f7a27b1e319d0ab31d3421af6ba499fce
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-07-01 18:13:24 +0530

    Update std:: randomize.v

--- SV_ASSIGNMENTS/Constraints/Random variables/std:: randomize.v
+++ SV_ASSIGNMENTS/Constraints/Random variables/std:: randomize.v
@@ -1,1 +1,16 @@
 
+class rand_fun;
+   bit [3:0]a;
+   logic[2:0]b;
+endclass
+
+module ab;
+  rand_fun rf;
+  initial begin
+    rf=new();
+    for(int i=0;i<20;i=i+1)begin
+      std::randomize(rf.a);
+      $display("Randomized value of a is=%d & Randomized value of b is%d ",rf.a,rf.b);
+    end
+  end
+endmodule
