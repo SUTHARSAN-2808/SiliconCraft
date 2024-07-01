3632baf424d4327ac80bb2e2ae635f8e116a165c
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-07-01 17:51:15 +0530

    Update Rand and Randc.v

--- SV_ASSIGNMENTS/Constraints/Random variables/Rand and Randc.v
+++ SV_ASSIGNMENTS/Constraints/Random variables/Rand and Randc.v
@@ -1,1 +1,16 @@
+//rand function and randc function
+class rand_fun;
+  rand bit [3:0]a;
+  randc logic[2:0]b; //randc give rand values and repeats on completing one cycle
+endclass
 
+module ab;
+  rand_fun rf;
+  initial begin
+    rf=new();
+    for(int i=0;i<20;i=i+1)begin
+      rf.randomize();
+      $display("Randomized value of a is=%d & Randomized value of b is%d ",rf.a,rf.b);
+    end
+  end
+endmodule
