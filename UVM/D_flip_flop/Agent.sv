110d300b20d443c4563e22ba1d9d16b8337fc2a1
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-07-30 14:08:59 +0530

    Update Agent.sv

--- UVM/D_flip_flop/Agent.sv
+++ UVM/D_flip_flop/Agent.sv
@@ -1,1 +1,35 @@
 
+
+// `include "seq_item.sv"
+//  `include "sequence.sv"
+ `include "sequencer.sv"
+`include "Monitor.sv"
+ `include "Driver.sv"
+
+
+class agent extends uvm_agent;
+
+  `uvm_component_utils(agent)
+  sequencer seqr;
+  driver drv;
+  monitor mon;
+
+  function new(string name = "agent", uvm_component parent);
+    super.new(name, parent);
+  endfunction
+
+  function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
+    seqr = sequencer::type_id::create("seqr", this);
+    drv = driver::type_id::create("drv", this);
+    mon=monitor::type_id::create("mon",this);
+  endfunction
+
+  function void connect_phase(uvm_phase phase);
+     if(get_is_active() == UVM_ACTIVE) begin
+       drv.seq_item_port.connect(seqr.seq_item_export);
+     end
+  endfunction
+
+endclass
+
