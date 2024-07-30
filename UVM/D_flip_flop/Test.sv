419bf6ef02df70f08499b0afa96433538e6e8d32
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-07-30 14:10:38 +0530

    Update Test.sv

--- UVM/D_flip_flop/Test.sv
+++ UVM/D_flip_flop/Test.sv
@@ -1,1 +1,33 @@
+`include "seq_item.sv"
+`include "sequence.sv"
+`include "Environment.sv"
+
+
+class test extends uvm_test;
+
+  environment env;
+       sequ seq;
+  `uvm_component_utils(test)
+
+  function new(string name = "test", uvm_component parent);
+    super.new(name, parent);
+  endfunction
+
+  function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
+    env = environment::type_id::create("env", this);
+  endfunction
+
+  task run_phase(uvm_phase phase);
+    phase.raise_objection(this);
+    `uvm_info(get_type_name(), "TEst sequence", UVM_HIGH);
+    seq = sequ::type_id::create("seq");
+    repeat (2) begin
+      #5 seq.start(env.agt.seqr);
+      #400;
+    end
+    phase.drop_objection(this);
+  endtask
+
+endclass
 
