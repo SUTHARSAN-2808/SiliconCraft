2ae4221100fe39016a698fe2c98fc8014e2434cf
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-07-30 14:03:59 +0530

    Update seq_item.sv

--- UVM/D_flip_flop/seq_item.sv
+++ UVM/D_flip_flop/seq_item.sv
@@ -1,1 +1,24 @@
 
+`include "uvm_macros.svh"
+`include "uvm_pkg.sv"
+
+import uvm_pkg::*;
+
+class seq_item extends uvm_sequence_item;
+
+  rand bit reset;
+   reg d;
+  rand bit q;
+
+  function new(string name = "seq_item");
+    super.new(name);
+  endfunction
+
+  `uvm_object_utils_begin(seq_item)
+    `uvm_field_int(reset, UVM_ALL_ON)
+    `uvm_field_int(d, UVM_ALL_ON)
+    `uvm_field_int(q, UVM_ALL_ON)
+  `uvm_object_utils_end
+
+endclass
+
