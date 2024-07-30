8825ae3ae699adf1ad375fd9aea897c1589d62b2
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-07-30 14:04:35 +0530

    Update sequence.sv

--- UVM/D_flip_flop/sequence.sv
+++ UVM/D_flip_flop/sequence.sv
@@ -1,1 +1,23 @@
 
+class sequ extends uvm_sequence#(seq_item);
+
+  `uvm_object_utils(sequ)
+  seq_item pkt;
+
+  function new(string name = "sequ");
+    super.new(name);
+  endfunction
+
+   task body();
+    forever begin
+      `uvm_info(get_type_name(), "Starting sequence", UVM_HIGH);
+      pkt = seq_item::type_id::create("pkt");
+//       start_item(pkt);
+ //        pkt.randomize();
+//       finish_item(pkt);
+      `uvm_do(pkt)
+    end
+  endtask
+
+endclass
+
