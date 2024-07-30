42de9d07bd58bd8d1fc2a9d94e5d846a9e450a46
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-07-30 14:02:39 +0530

    Update Design.sv

--- UVM/D_flip_flop/Design.sv
+++ UVM/D_flip_flop/Design.sv
@@ -1,1 +1,15 @@
+//d-flipflop
+module dff(input clk,
+           input reset,
+           input q,
+            output reg d);
+ 
+  always @(posedge clk)begin
+    if(reset==1'b1)
+      d<=1'b0;
+    else 
+      d<=q;
+  end
+endmodule
+
 
