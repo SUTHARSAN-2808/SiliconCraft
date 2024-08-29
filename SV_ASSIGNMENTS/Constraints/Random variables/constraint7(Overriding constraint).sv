31468c7c9b8830d4b8ed7c1c1d8954b278534b66
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-08-29 16:41:00 +0530

    Update constraint7(Overriding constraint).sv

--- SV_ASSIGNMENTS/Constraints/Random variables/constraint7(Overriding constraint).sv
+++ SV_ASSIGNMENTS/Constraints/Random variables/constraint7(Overriding constraint).sv
@@ -1,1 +1,15 @@
+class cyclic_dependencies;
+  randc bit[7:0] addr;
+  constraint over_c { addr > 100 && addr < 50; } 
+endclass
 
+module m1;
+  cyclic_dependencies cycle;
+  initial begin
+    cycle=new();
+    repeat(10)begin
+      cycle.randomize();
+      $display("addr = %d ",cycle.addr);
+    end
+  end
+endmodule
