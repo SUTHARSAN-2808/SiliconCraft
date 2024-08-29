69636542ac52249e590ff89bf3c8166cfa4371a6
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-08-29 16:45:39 +0530

    Update constraint8(soft constraint).sv

--- SV_ASSIGNMENTS/Constraints/Random variables/constraint8(soft constraint).sv
+++ SV_ASSIGNMENTS/Constraints/Random variables/constraint8(soft constraint).sv
@@ -1,1 +1,16 @@
+class cyclic_dependencies;
+  randc bit[7:0] addr;
+  constraint c1{ soft addr==50;}
+  constraint c2{addr==100;}
+endclass
 
+module m1;
+  cyclic_dependencies cycle;
+  initial begin
+    cycle=new();
+    repeat(10)begin
+      cycle.randomize();
+      $display("addr = %d ",cycle.addr);
+    end
+  end
+endmodule
