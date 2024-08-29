012cc08675771b6f6ea90486ef673331228bf038
SUTHARSAN-2808 <130533634+SUTHARSAN-2808@users.noreply.github.com>
2024-08-29 14:48:36 +0530

    Update constraint1.sv

--- SV_ASSIGNMENTS/Constraints/Random variables/constraint1.sv
+++ SV_ASSIGNMENTS/Constraints/Random variables/constraint1.sv
@@ -1,1 +1,15 @@
+class cyclic_dependencies;
+  randc bit[7:0] data,addr;
+  constraint c1{addr<30 ; data<addr;}
+endclass
 
+module m1;
+  cyclic_dependencies cycle;
+  initial begin
+    cycle=new();
+    repeat(10)begin
+      cycle.randomize();
+      $display("addr = %d data =%d",cycle.addr,cycle.data);
+    end
+  end
+endmodule
